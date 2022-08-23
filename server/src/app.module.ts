bb6ab1bccdb0351da1382836492d665543921d84
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-08-24 00:00:08 +0100

    import userModule, authModule and typeOrmModule
    create database with typeOrm and user table

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [UserModule, TypeOrmModule.forRoot({
+		type: 'postgres',
+		host: '127.0.0.1',
+		port: 5432,
+		username: 'Conanyedo',
+		password: 'nestjs',
+		database: 'nestjs',
+		// entities: [User],
+		autoLoadEntities: true,
+		synchronize: true,
+		// logging: true, // log sql queries
+	}), AuthModule]
 })
-export class AppModule {}
+export class AppModule { }
