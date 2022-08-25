420aa05c570f7f5963e102a48bada71868a54abf
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-08-25 21:36:54 +0100

    import needed Modules and providers
    provide intraStrategy and googleStrategy to be used as guards
    import user and jwt modules to use their services

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -2,17 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './jwt.strategy';
-import { IntraStrategy } from './intra.strategy';
+import { IntraStrategy } from './intra-auth.strategy';
 import { UserModule } from 'src/user/user.module';
+import { JwtAuthModule } from 'src/jwt-auth/jwt-auth.module';
+import { GoogleOauthStrategy } from './google-oauth.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, IntraStrategy, JwtStrategy],
-	imports: [UserModule, PassportModule, JwtModule.register({
-		secret: 'jwtConstants.secret',
-		signOptions: { expiresIn: '1d' }
-	})]
+  providers: [AuthService, IntraStrategy, GoogleOauthStrategy],
+	imports: [UserModule, JwtAuthModule, PassportModule]
 })
 export class AuthModule {}
