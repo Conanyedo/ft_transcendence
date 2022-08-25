2ab02c85965a5dd7d3cb8ab1b45684c9b400d3e0
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-08-25 21:19:08 +0100

    create intraStrategy to authenticate using 42intra
    set clientID, clientSecret and callbackURL with configService to get
    them from .env file
    
    method validate: after validation succeed validate get called with
    request params to use, create user from profile returned and add it to
    db if needed, then return it

--- server/src/auth/intra-auth.guard.ts
+++ server/src/auth/intra-auth.guard.ts
@@ -2,4 +2,4 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class IntraAuthGuard extends AuthGuard('42') {}
+export class IntraAuthGuard extends AuthGuard('42') { }
