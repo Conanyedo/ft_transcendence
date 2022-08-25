23e69dfff2088a1767487fd751d67c6cecd741f3
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-08-25 20:58:37 +0100

    create jwtStrategy for authorization
    get secretKey from configService

---
+++ server/src/jwt-auth/jwt-auth.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtAuthStrategy extends PassportStrategy(Strategy) {
+	constructor(private readonly configService: ConfigService) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: false,
+			secretOrKey: configService.get('JWT_SECRET_KEY'),
+		});
+	}
+
+	async validate(payload: any) {
+		return { id: payload.sub, login: payload.login }
+	}
+}
