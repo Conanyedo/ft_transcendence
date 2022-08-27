b48f5dca30127b63c74e02bada904cae4cecf35c
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-08-27 22:33:04 +0100

    install otplib and qrcode packages

--- client/components/Appcontent.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useRef, useState } from "react";
-import Section from "./section";
-
-const AppContent = (props: { elm: HTMLAllCollection }) => {
-	return <Section elm={props.elm} />;
-};
-
-export default AppContent;
