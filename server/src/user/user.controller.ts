a2f3c6aea2e89491f8729d50fc104b96a667bae2
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-08-27 21:57:02 +0100

    server

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Post, Request, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
-import { createUserDto } from './dto/createUser.dto';
+import { userDto } from './user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -7,9 +7,9 @@ export class UserController {
 
 	constructor(private readonly userService: UserService) {}
 
-	@Post('/register')
-	@UsePipes(ValidationPipe)
-	async doUserRegistration(@Body() userRegister: createUserDto) {
-		return await this.userService.doUserRegistration(userRegister);
-	}
+	// @Post('/register')
+	// @UsePipes(ValidationPipe)
+	// async doUserRegistration(@Body() userRegister: userDto) {
+	// 	return await this.userService.registerUser(userRegister);
+	// }
 }
