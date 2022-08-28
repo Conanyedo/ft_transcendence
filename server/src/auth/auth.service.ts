d5def4718cad84921e3bc1896fd9491d42e194cb
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-08-28 02:00:59 +0100

    add methods to be used by controller and strategies
    checkUserExit: to add user to database if doesnt exist
    setJWTCookie: to add token to response cookie
    checkUserAth: if 2fa enabled authentication is not completed, if not
    redirect to home and set user status as authenticated in database
    logout: expire jwt and set user status as unauthenticated in database
    2fa methods to generate and check if 2fa succeeded, if so set user as
    authenticated

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { JwtAuthService } from 'src/jwt-auth/jwt-auth.service';
-import { createUserDto } from 'src/user/dto/createUser.dto';
+import { userDto } from 'src/user/user.dto';
 import { UserService } from 'src/user/user.service';
+import { authenticator } from 'otplib';
+import { toDataURL } from 'qrcode';
 
 @Injectable()
 export class AuthService {
@@ -12,14 +14,61 @@ export class AuthService {
 		private readonly jwtAuthService: JwtAuthService,
 	) { }
 
-	async checkUserExist(user: createUserDto) {
-		let getUser = await this.userService.getUserById(user.id);
+	async checkUserExist(user: userDto): Promise<userDto> {
+		let getUser = await this.userService.getUserByEmail(user.email);
 		if (!getUser)
-			getUser = await this.userService.doUserRegistration(user);
+			getUser = await this.userService.registerUser(user);
+		return { ...getUser };
 	}
 
-	setToken(req: Request) {
-		const accessToken = this.jwtAuthService.login(<createUserDto>req.user);
-		return accessToken;
+	setJWTCookie(user: userDto, res: Response) {
+		const accessToken = this.jwtAuthService.login(user);
+		res.cookie('jwt', accessToken);
+	}
+
+	async checkUserAuthentication(user: userDto, res: Response) {
+		if (user.is2faEnabled)
+			return res.redirect('http://localhost:3000/?_2fa=true');
+		res.redirect('http://localhost:3000/');
+		await this.userService.setUserAuthenticated(user.id, true);
+	}
+
+	async logout(user: userDto, res: Response) {
+		res.cookie('jwt', '', { maxAge: 1 });
+		res.redirect('http://localhost:3000/');
+		const getUser = await this.userService.setUserAuthenticated(user.id, false);
+		console.log('Logout', getUser);
+	}
+
+
+	// 2FactorAuthentication
+
+	async generate2faSecret(user: userDto) {
+		const secret = authenticator.generateSecret();
+		console.log('2fa Secret:', secret);
+		const otpAuthUrl = authenticator.keyuri(
+			user.email,
+			'Transcendence',
+			secret,
+		);
+		await this.userService.set2faSecret(user.id, secret);
+		return otpAuthUrl;
+	}
+
+	async generateQrCodeDataURL(otpAuthUrl: string) {
+		return await toDataURL(otpAuthUrl);
+	}
+
+	is2faCodeValid(code: string, user: userDto) {
+    return authenticator.verify({
+      token: code,
+      secret: user._2faSecret,
+    });
+  }
+
+	async authenticateUser(user: userDto) {
+		let getUser = await this.userService.getUserById(user.id);
+		getUser = await this.userService.setUserAuthenticated(getUser.id, true);
+		return { ...getUser };
 	}
 }
