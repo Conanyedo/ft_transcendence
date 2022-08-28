438b2130064ee0dd67ea938000200b001e486faf
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-08-28 01:34:00 +0100

    add methods to be used by authModule
    getter to get user by id or email
    set user athentication status as true or false in database
    set 2faSecret in database

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { createUserDto } from './dto/createUser.dto';
+import { userDto } from './user.dto';
 import { User } from './user.entity';
 
 @Injectable()
@@ -12,19 +12,38 @@ export class UserService {
 		private userRepository: Repository<User>
 	) { }
 
-	async doUserRegistration(userRegister: createUserDto) {
+	async registerUser(newUser: userDto) {
 		const user = new User();
-		user.id = userRegister.id;
-		user.login = userRegister.login;
-		user.email = userRegister.email;
-		user.fullname = userRegister.fullname;
-		user.avatar = userRegister.avatar;
-		const newUser = await this.userRepository.save(user);
+		user.login = newUser.login;
+		user.email = newUser.email;
+		user.fullname = newUser.fullname;
+		user.avatar = newUser.avatar;
+		return await this.userRepository.save(user);
+	}
 
-		return newUser;
+	async getUserByEmail(email: string) {
+		return await this.userRepository.findOne({ where: { email } });
 	}
 
 	async getUserById(id: string) {
 		return await this.userRepository.findOne({ where: { id } });
 	}
+
+	async setUserAuthenticated(id: string, status: boolean) {
+		const user = await this.userRepository.findOne({ where: { id } });
+		user.isAuthenticated = status;
+		return await this.userRepository.save(user);
+	}
+
+	async set2faSecret(id: string, secret: string) {
+		const user = await this.userRepository.findOne({ where: { id } });
+		user._2faSecret = secret;
+		await this.userRepository.save(user);
+	}
+
+	// async turnOn2fa(id: string) {
+	// 	const user = await this.userRepository.findOne({ where: { id } });
+	// 	user.is2faEnabled = true;
+	// 	await this.userRepository.save(user);
+	// }
 }
