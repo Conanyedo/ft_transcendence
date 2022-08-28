a765d987449d246cf7530c0cf2a64ad411275271
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-08-28 01:56:32 +0100

    get user from db using the sub in the payload
    inject userService to use method getUserById

--- server/src/jwt-auth/jwt-auth.strategy.ts
+++ server/src/jwt-auth/jwt-auth.strategy.ts
@@ -2,10 +2,15 @@ import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { ConfigService } from '@nestjs/config';
+import { UserService } from "src/user/user.service";
+import { userDto } from "src/user/user.dto";
 
 @Injectable()
 export class JwtAuthStrategy extends PassportStrategy(Strategy) {
-	constructor(private readonly configService: ConfigService) {
+	constructor(
+		private readonly userService: UserService,
+		private readonly configService: ConfigService
+	) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			ignoreExpiration: false,
@@ -14,6 +19,8 @@ export class JwtAuthStrategy extends PassportStrategy(Strategy) {
 	}
 
 	async validate(payload: any) {
-		return { id: payload.sub, login: payload.login }
+		const getUser = await this.userService.getUserById(payload.sub);
+		const user: userDto = { ...getUser };
+		return user;
 	}
 }
