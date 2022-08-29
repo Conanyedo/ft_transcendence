2822851f65dba64ba0c9a3d916bd2a59ef48674b
Abouelwafa Choaib <cabouelw@student.1337.ma>
2022-08-29 17:06:25 +0100

    adjusting 2 factor authentication & fixing backend's path

--- client/components/profile/settings.tsx
+++ client/components/profile/settings.tsx
@@ -1,7 +1,7 @@
 import classes from "../../styles/setting.module.css";
 import Image from "next/image";
 import CrossIcon from "../../public/FriendIcons/Cross.svg";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { useDispatch, useSelector } from "react-redux";
 import { HideSettings, Settings } from "../store/UI-Slice";
@@ -9,145 +9,205 @@ import { getCookie } from "cookies-next";
 import axios from "axios";
 import MsgSlideUp from "../slideUpMsg";
 
-export const OTP_Input: React.FC<{inputValue: string, setInputValue: any}> = (props) => {
-	const ref_1 = useRef(null);
-	const ref_2 = useRef(null);
-	const ref_3 = useRef(null);
-	const ref_4 = useRef(null);
-	const ref_5 = useRef(null);
-	const ref_6 = useRef(null);
-	const refTable = [ref_1, ref_2, ref_3, ref_4, ref_5, ref_6];
-	const changeHandler = (e: any) => {
-		if (e.target.value.length === 0) {
-			if (props.inputValue.length === 0) return;
-			if (e.target.alt != 1)
-				refTable[Number(e.target.alt - 2)].current!.focus();
-				props.setInputValue((v:any) => v.substring(0, v.length - 1));
-		} else {
-			if (props.inputValue.length === 6) return;
-			props.setInputValue((value:any) => value.concat(e.target.value));
-			if (e.target.alt < 6) refTable[e.target.alt].current!.focus();
-		}
-	};
-	return (
-		<div className={classes.inputContainer}>
-			<input
-				autoFocus
-				alt="1"
-				className={classes.input}
-				maxLength={1}
-				onChange={changeHandler}
-				ref={ref_1}
-			/>
-			<input
-				alt="2"
-				className={classes.input}
-				maxLength={1}
-				onChange={changeHandler}
-				ref={ref_2}
-			/>
-			<input
-				alt="3"
-				className={classes.input}
-				maxLength={1}
-				onChange={changeHandler}
-				ref={ref_3}
-			/>
-			<input
-				alt="4"
-				className={classes.input}
-				maxLength={1}
-				onChange={changeHandler}
-				ref={ref_4}
-			/>
-			<input
-				alt="5"
-				className={classes.input}
-				maxLength={1}
-				onChange={changeHandler}
-				ref={ref_5}
-			/>
-			<input
-				alt="6"
-				className={classes.input}
-				maxLength={1}
-				onChange={changeHandler}
-				ref={ref_6}
-			/>
-		</div>
-	);
+import OtpInput from "react-otp-input";
+import Grid from "@material-ui/core/Grid";
+
+const checkCode = async (code: string) => {
+	const token = getCookie("jwt");
+	const params = new URLSearchParams();
+	params.append("code", code);
+	try {
+		await axios({
+			method: "post",
+			url: `http://localhost:5000/auth/2faValidate`,
+			data: params,
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+			withCredentials: true,
+		});
+		return true;
+	} catch (err) {
+		return false;
+	}
 };
 
-const Setting: React.FC = () => {
+const getCode = async () => {
 	const token = getCookie("jwt");
-	const dispatch = useDispatch();
-	const [isValid, setIsValid] = useState(false);
-	const [unValid, setUnValid] = useState(false);
-	const [isOn, setIsOn] = useState(false);
-	const data = async (api: string) => {
+	const params = new URLSearchParams();
+	params.append("is2faEnabled", "true");
+	try {
+		const result = await axios({
+			method: "post",
+			url: `http://localhost:5000/auth/2faEnabling`,
+			data: params,
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+			withCredentials: true,
+		});
+		return result.data;
+	} catch (err) {
+		return "#";
+	}
+};
+
+const variants = {
+	open: { scale: 1 },
+	closed: { scale: 0 },
+	hide: { opacity: 0, scale: 0 },
+};
+const spring = {
+	type: "spring",
+	stiffness: 700,
+	damping: 30,
+};
+
+const disablehandler = async (status: string) => {
+	const token = getCookie("jwt");
+	const params = new URLSearchParams();
+	params.append("is2faEnabled", status);
+	try {
+		const result = await axios({
+			method: "post",
+			url: `http://localhost:5000/auth/2faEnabling`,
+			data: params,
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+			withCredentials: true,
+		});
+		return result.data;
+	} catch {
+	}
+};
+
+const isEnabledhandler = (set: any, setP: any) => {
+	let res = false;
+	const check = async () => {
+		const token = getCookie("jwt");
 		try {
-			const result = await axios.get(`http://localhost:5000/auth/${api}`, {
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-				withCredentials: true,
-			});
-			setIsOn(result.data);
-		} catch (err) {
-            return err;
-		}
-	};
-	useEffect(() => {
-		data('is2faEnabled');
-	}, [])
-	const displayCard = useSelector(Settings);
-	const variants = {
-		open: { scale: 1 },
-		closed: { scale: 0 },
-		hide: { opacity: 0, scale: 0 },
-	};
-	const spring = {
-		type: "spring",
-		stiffness: 700,
-		damping: 30,
-	};
-	const [nextPage, setNextPage] = useState(true);
-	const [QRcode, setQRcode] = useState('#');
-	const toggleSwitch = () => {
-		setIsOn(!isOn);
-	};
-	const toggleHandler = () => {
-		const data = async (api: string) => {
-			try {
-				const result = await axios.get(`http://localhost:5000/auth/${api}`, {
+			const result = await axios.get(
+				`http://localhost:5000/auth/is2faEnabled`,
+				{
 					headers: {
 						Authorization: `Bearer ${token}`,
 					},
 					withCredentials: true,
-				});
-				setQRcode(result.data);
-			} catch (err) {
-				return err;
-			}
-		};
-		if (nextPage) {
-			setNextPage(false)
-			data('2fa');
+				}
+			);
+			set(result.data);
+			setP(result.data);
+		} catch {
 		}
-		else if (!nextPage) {
-			setTimeout(()=> {
-				setIsValid(false);
-				dispatch(HideSettings());
-			}, 3000);
-			setIsValid(true);
-			setNextPage(true);
+	};
+	check();
+	return res;
+};
+
+const FirstPage: React.FC<{ isOn: boolean; toggleSwitch: () => void }> = (
+	props
+) => {
+	return (
+		<>
+			<div className={classes.SwitchContainer}>
+				<span className={classes.title}>2 Factor Authentication</span>
+				<div
+					className={classes.switch}
+					data-ison={props.isOn}
+					onClick={props.toggleSwitch}
+				>
+					<motion.div
+						className={classes.handle}
+						layout
+						transition={spring}
+					/>
+				</div>
+			</div>
+		</>
+	);
+};
+
+const SecondPage: React.FC<{ QRcode: string, inputValue:string, codeFailed:boolean, setIsOn: any, setInputValue: any }> = (
+	props
+) => {
+	return (
+		<>
+			<img src={props.QRcode} />
+			<span>Please entre the OTP</span>
+			<Grid >
+				<OtpInput
+					value={props.inputValue}
+					numInputs={6}
+					inputStyle={{
+						width: "3rem",
+						height: "3rem",
+						margin: "0 .5rem",
+						fontSize: "1.5rem",
+						borderRadius: 4,
+						border: `1px solid ${props.codeFailed ? "red" : "black"}`,
+					}}
+					onChange={(e: any) => {
+						props.setIsOn(e.length === 6);
+						props.setInputValue(e);
+					}}
+					shouldAutoFocus={true}
+					isInputNum={true}
+				/>
+			</Grid>
+		</>
+	);
+};
+
+const Setting: React.FC = () => {
+	const dispatch = useDispatch();
+	const displayCard = useSelector(Settings);
+	const [isValid, setIsValid] = useState(false);
+	const [change, setchange] = useState(false);
+	const [codeFailed, setcodeFailed] = useState(false);
+	const [prevstat, setprevstat] = useState(false);
+	const [isOn, setIsOn] = useState(false);
+	const [QRcode, setQRcode] = useState("#");
+	useEffect(() => {
+		isEnabledhandler(setprevstat, setIsOn);
+	}, []);
+	const toggleSwitch = () => {
+		setIsOn(!isOn);
+		setchange(true);
+	};
+	const toggleHandler = async () => {
+		if (QRcode === "#") {
+			const qrCode: string = await disablehandler('true');
+			setQRcode(qrCode);
+		} else if (QRcode !== "#" && inputValue.length === 6) {
+			const res: boolean = await checkCode(inputValue);
+			setIsValid(res);
+			setcodeFailed(!res);
+			if (res) {
+				setTimeout(() => {
+					dispatch(HideSettings());
+					setIsValid(false);
+					setcodeFailed(false);
+				}, 3000);
+			}
 		}
 	};
 	const [inputValue, setInputValue] = useState("");
+	const handlerDisable = async () => {
+		await disablehandler('false');
+		setIsValid(true);
+		setTimeout(() => {
+			dispatch(HideSettings());
+			setIsValid(false);
+			setcodeFailed(false);
+		}, 3000);
+	};
+
 	return (
 		<>
-		{isValid && <MsgSlideUp msg='Done' colorCtn="#31BAAE" colorMsg="#ECF5FF" />}
-		{unValid && <MsgSlideUp msg='Try Again' colorCtn="#FF6482" colorMsg="#ECF5FF" />}
+			{isValid && (
+				<MsgSlideUp msg="Done" colorCtn="#31BAAE" colorMsg="#ECF5FF" />
+			)}
 			{!isValid && displayCard && (
 				<div className={classes.background}>
 					<div className={classes.settingContainer}>
@@ -155,56 +215,53 @@ const Setting: React.FC = () => {
 							<span>Settings</span>
 							<div
 								className={classes.cross}
-								onClick={() => dispatch(HideSettings())}
+								onClick={() => {
+									setIsValid(false);
+									setIsOn(false);
+									dispatch(HideSettings());
+								}}
 							>
-								<Image src={CrossIcon} width='72' height='72' />
+								<Image src={CrossIcon} width="72" height="72" />
 							</div>
 						</div>
 						<div className={classes.pages}>
-							{nextPage && (
-								<div className={classes.SwitchContainer}>
-									<span className={classes.title}>
-										2 Factor Authentication
-									</span>
-									<div
-										className={classes.switch}
-										data-isOn={isOn}
-										onClick={toggleSwitch}
-									>
-										<motion.div
-											className={classes.handle}
-											layout
-											transition={spring}
-										/>
-									</div>
-								</div>
+							{QRcode === "#" && (
+								<FirstPage
+									isOn={isOn}
+									toggleSwitch={toggleSwitch}
+								/>
 							)}
 							<motion.div
 								defaultValue={"closed"}
 								animate={
-									!nextPage
+									QRcode !== "#"
 										? "open"
-										: nextPage
+										: QRcode === "#"
 										? "hide"
 										: "closed"
 								}
 								variants={variants}
 								className={classes.qrCodeContainer}
 							>
-								{!nextPage && (
-									<>
-									<img src={QRcode}/>
-										<span>Plaise type the 6 Number</span>
-										<OTP_Input inputValue={inputValue} setInputValue={setInputValue} />
-									</>
+								{QRcode !== "#" && (
+									<SecondPage QRcode={QRcode} inputValue={inputValue} setIsOn={setIsOn} setInputValue={setInputValue} codeFailed={codeFailed} />
 								)}
 							</motion.div>
 						</div>
 						<div
-							className={`${classes.btn} ${isOn ? classes.btnNext : ''}`}
-							onClick={toggleHandler}
+							className={`${classes.btn} ${
+								prevstat !== isOn ? classes.btnNext : ""
+							}`}
+							onClick={
+								(QRcode === "#" && !isOn && handlerDisable) ||
+								toggleHandler
+							}
 						>
-							{nextPage ? "Next" : "Save"}
+							{change && prevstat !== isOn && isOn
+								? QRcode === "#"
+									? "Next"
+									: "Save"
+								: "Confirm"}
 						</div>
 					</div>
 				</div>
