f9d055614fc4c2a542ef7bafa52598416666439e
Abouelwafa Choaib <cabouelw@student.1337.ma>
2022-08-29 17:01:54 +0100

    check if the user has 2FA or not and check if 2FA Code is valid or Not

--- client/components/profile/FactorAuth.tsx
+++ client/components/profile/FactorAuth.tsx
@@ -1,11 +1,46 @@
 import classes from "../../styles/factorAuth.module.css";
 import CrossIcon from "../../public/FriendIcons/Cross.svg";
 import Image from "next/image";
-import { OTP_Input } from "./settings";
+import OtpInput from "react-otp-input";
 import { useState } from "react";
+import { Grid } from "@material-ui/core";
+import { getCookie } from "cookies-next";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+const checkCode = async (code: string) => {
+	const token = getCookie("jwt");
+	const params = new URLSearchParams();
+	params.append("code", code);
+	try {
+		await axios({
+			method: "post",
+			url: `http://localhost:5000/auth/2faLogin`,
+			data: params,
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+			withCredentials: true,
+		});
+		return true;
+	} catch (err) {
+		return false;
+	}
+};
+
 
 const FactorAuth = () => {
+			console.log('hna');
+	const router = useRouter();
 	const [inputValue, setInputValue] = useState("");
+	const [isError, setisError] = useState(false);
+	const CheckHandler = async () => {
+		if (await checkCode(inputValue)){
+			
+			router.push('/profile');}
+		else
+			setisError(true)
+	}
 	return (
 		<>
 			<div className={classes.background}>
@@ -17,15 +52,32 @@ const FactorAuth = () => {
 						</div>
 					</div>
 					<div className={classes.inputContainer}>
-						<OTP_Input
-							inputValue={inputValue}
-							setInputValue={setInputValue}
-						/>
+						<Grid>
+							<OtpInput
+								value={inputValue}
+								numInputs={6}
+								inputStyle={{
+									width: "3rem",
+									height: "3rem",
+									margin: "0 .5rem",
+									fontSize: "1.5rem",
+									borderRadius: 4,
+									border: `1px solid ${
+										isError ? "red" : "black"
+									}`,
+								}}
+								onChange={(e: any) => {
+									setInputValue(e);
+								}}
+								shouldAutoFocus={true}
+								isInputNum={true}
+							/>
+						</Grid>
 					</div>
 					<div
 						className={`${classes.btn} ${
 							inputValue.length === 6 ? classes.btnNext : ""
-						}`}
+						}`} onClick={CheckHandler}
 					>
 						Confirmer
 					</div>
--- client/components/skeleton.tsx
+++ client/components/skeleton.tsx
@@ -1,12 +1,13 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Header from "./Header/Header";
 import SideNav from "./Header/sideNav";
 import classesNav from "../styles/sideNav.module.css";
-import { useRouter } from "next/router";
+import { NextRouter, useRouter } from "next/router";
 import ProfileInfoEdit from "./profile/ProfileInfoEdit";
 import { useAppDispatch, useAppSelector } from "./store/hooks";
 import {
 	HideErrorMsg,
+	Settings,
 	Toggle,
 	ToggleErrorValue,
 	ToggleValue,
@@ -16,14 +17,36 @@ import classes from "../styles/Profile.module.css";
 import { getCookie } from "cookies-next";
 import Setting from "./profile/settings";
 import MsgSlideUp from "./slideUpMsg";
+import axios from "axios";
+
+const fetchData = async (set: any, router: NextRouter) => {
+	const token = getCookie("jwt");
+	const requestOptions = {
+		Authorization: `Bearer ${token}`,
+	};
+	await axios
+		.get(`http://localhost:5000/auth/isAuthenticated`, {
+			headers: requestOptions,
+			withCredentials: true,
+			method: "GET",
+		})
+		.then((res) => {
+			set(true);
+		})
+		.catch((err) => {
+			router.replace("/");
+		});
+};
 
 const Skeleton = (props: { elm: any }) => {
 	const ctn = useRouter();
-	const token = getCookie("jwt");
-	if (!token) {
-		ctn.replace("/");
-		return <></>;
-	}
+	const dispatch = useAppDispatch();
+	const displayCard = useAppSelector(ToggleValue);
+	const displaymsg = useAppSelector(Settings);
+	const ShowError = useAppSelector(ToggleErrorValue);
+	const [isAuthenticated, setisAuthenticated] = useState(false);
+	const [posIndicator, setPosIndicator] = useState(classesNav.hide);
+	
 	const NamePage = "/" + ctn.pathname.split("/")[1];
 	const navBarHandler = (page: string) => {
 		setPosIndicator(() => {
@@ -34,24 +57,19 @@ const Skeleton = (props: { elm: any }) => {
 			return classesNav.hide;
 		});
 	};
-	const [posIndicator, setPosIndicator] = useState(() => {
-		if (NamePage === "/profile") return classesNav.profilePos;
-		if (NamePage === "/live-games") return classesNav.liveGamePos;
-		if (NamePage === "/game") return classesNav.gamePos;
-		if (NamePage === "/chat") return classesNav.chatPos;
-		return classesNav.hide;
-	});
-
-	const displayCard = useAppSelector(ToggleValue);
-	const dispatch = useAppDispatch();
+	useEffect(() => {
+		fetchData(setisAuthenticated, ctn);
+		navBarHandler(NamePage);
+	}, []);
+	if (!isAuthenticated) {
+		return <></>;
+	}
 	const toggleHandler = () => dispatch(Toggle());
-	const ShowError = useAppSelector(ToggleErrorValue);
 	if (ShowError) {
 		setTimeout(() => {
 			dispatch(HideErrorMsg());
 		}, 3000);
 	}
-	// useEffect()
 	return (
 		<>
 			<style global jsx>{`
@@ -59,8 +77,14 @@ const Skeleton = (props: { elm: any }) => {
 					height: 100%;
 				}
 			`}</style>
-			{ShowError && <MsgSlideUp msg='User Not Found' colorCtn="#FF6482" colorMsg="#ECF5FF" /> }
-			<Setting />
+			{ShowError && (
+				<MsgSlideUp
+					msg="User Not Found"
+					colorCtn="#FF6482"
+					colorMsg="#ECF5FF"
+				/>
+			)}
+			{displaymsg && <Setting />}
 			{displayCard && <ProfileInfoEdit setTagle={toggleHandler} />}
 			<Header setPos={navBarHandler} />
 			<SideNav onNav={navBarHandler} currentPos={posIndicator} />
