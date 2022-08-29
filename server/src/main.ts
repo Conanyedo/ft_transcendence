aa051a6a46f541d10c1de51a61e90e5783556dcc
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-08-29 10:14:32 +0100

    enabel cors with specifying origin

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.enableCors();
+	app.enableCors({
+		origin: 'http://localhost:3000',
+		credentials: true
+	});
 	await app.listen(5000);
 }
 bootstrap();
