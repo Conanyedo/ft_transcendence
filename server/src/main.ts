465dda445c9149db36d7d91d3448d79ade9bdd27
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-08-30 22:48:32 +0100

    add ValidationPipe globaly

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -8,6 +9,7 @@ async function bootstrap() {
 		origin: 'http://localhost:3000',
 		credentials: true
 	});
+	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 	await app.listen(5000);
 }
 bootstrap();
