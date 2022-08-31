539af650924fb4a59c44efb93bdb44b5d524fb86
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-08-31 23:45:51 +0100

    import jwtAuthModule and add stats entity

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -3,11 +3,13 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { Stats } from './stats.entity';
+import { JwtAuthModule } from 'src/2fa-jwt/jwt/jwt-auth.module';
 
 @Module({
   providers: [UserService],
   controllers: [UserController],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Stats]), JwtAuthModule],
   exports: [UserService]
 })
 export class UserModule {}
