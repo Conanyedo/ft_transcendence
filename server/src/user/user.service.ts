aa8ae41501b723bfa689c8aa9aab518dd33020f1
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-08-31 23:53:00 +0100

    inject statsRepo to interact with stats table
    add more methods to add/update/fetch data from database

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Stats } from './stats.entity';
 import { userDto, userParitalDto } from './user.dto';
 import { User } from './user.entity';
 
@@ -9,15 +10,24 @@ export class UserService {
 
 	constructor(
 		@InjectRepository(User)
-		private userRepository: Repository<User>
+		private userRepository: Repository<User>,
+		@InjectRepository(Stats)
+		private statsRepository: Repository<Stats>,
 	) { }
 
 	async registerUser(newUser: userDto): Promise<userParitalDto> {
+		const stats: Stats = new Stats();
+		stats.rank = await this.userRepository
+			.createQueryBuilder('users')
+			.getCount();
+		console.log('user rank', stats.rank);
 		let user: User = new User();
 		user.login = newUser.login;
 		user.email = newUser.email;
 		user.fullname = newUser.fullname;
 		user.avatar = newUser.avatar;
+		user.stats = stats;
+		this.statsRepository.save(stats);
 		user = await this.userRepository.save(user);
 		const getUser: userParitalDto = {
 			id: user.id,
@@ -26,6 +36,14 @@ export class UserService {
 		return getUser;
 	}
 
+	// Edit Profile
+	async editProfile(id: string, fullname: string, avatar: string) {
+
+		if (fullname)
+			await this.setName(id, fullname);
+		if (avatar)
+			await this.setAvatar(id, `/server/uploads/${avatar}`);
+	}
 
 	// User Getters
 	async getPartialUser(email: string): Promise<userParitalDto> {
@@ -59,6 +77,26 @@ export class UserService {
 			.getOne();
 		return user.is2faEnabled;
 	}
+
+	async getUserInfo(id: string) {
+		const user: User = await this.userRepository
+			.createQueryBuilder('users')
+			.leftJoinAndSelect("users.stats", "stats")
+			.select(['users.fullname', 'users.avatar', 'stats.XP', 'stats.GP', 'stats.rank'])
+			.where('users.id = :id', { id: id })
+			.getOne();
+		return { ...user };
+	}
+
+	async getUserHeader(id: string) {
+		const user: User = await this.userRepository
+			.createQueryBuilder('users')
+			.leftJoinAndSelect("users.stats", "stats")
+			.select(['users.fullname', 'users.avatar'])
+			.where('users.id = :id', { id: id })
+			.getOne();
+		return { ...user };
+	}
 	// ------------------------------
 
 
@@ -83,6 +121,22 @@ export class UserService {
 		return await this.userRepository
 			.createQueryBuilder('users')
 			.update({ is2faEnabled: status })
+			.where('id = :id', { id: id })
+			.execute();
+	}
+
+	async setAvatar(id: string, avatar: string) {
+		return await this.userRepository
+			.createQueryBuilder('users')
+			.update({ avatar: avatar })
+			.where('id = :id', { id: id })
+			.execute();
+	}
+
+	async setName(id: string, name: string) {
+		return await this.userRepository
+			.createQueryBuilder('users')
+			.update({ fullname: name })
 			.where('id = :id', { id: id })
 			.execute();
 	}
