75412f86f62c37a70326b0084579801440c67e59
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-08-31 23:48:06 +0100

    create routes to get/post user data
    info route to get user profile info
    header route to get fullname and avatar only
    editProfile to update fullname and avatar

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,15 +1,34 @@
-import { Body, Controller, Get, Post, Request, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
-import { userDto } from './user.dto';
+import { Body, Controller, Get, Post, Request, UploadedFile, UseGuards, UseInterceptors, UsePipes, ValidationPipe } from '@nestjs/common';
+import { userDto, userParitalDto } from './user.dto';
 import { UserService } from './user.service';
+import { JwtAuthGuard } from '../2fa-jwt/jwt/jwt-auth.guard';
+import { User } from './user.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { uploadConfig } from 'src/config/upload.config';
 
 @Controller('user')
 export class UserController {
 
-	constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) { }
 
-	// @Post('/register')
-	// @UsePipes(ValidationPipe)
-	// async doUserRegistration(@Body() userRegister: userDto) {
-	// 	return await this.userService.registerUser(userRegister);
-	// }
+	@Get('/info')
+	@UseGuards(JwtAuthGuard)
+	async getUserInfo(@User() user: userParitalDto) {
+		return await this.userService.getUserInfo(user.id);
+	}
+
+	@Get('/header')
+	@UseGuards(JwtAuthGuard)
+	async getUserHeader(@User() user: userParitalDto) {
+		return await this.userService.getUserHeader(user.id);
+	}
+
+	@Post('/editProfile')
+	@UseGuards(JwtAuthGuard)
+	@UseInterceptors(FileInterceptor('avatar', uploadConfig))
+	async editProfile(@User() user: userParitalDto, @UploadedFile() avatar: Express.Multer.File, @Body('fullname') fullname: string, @Body('isDefault') isDefault: string) {
+		if (isDefault === 'false')
+			avatar = undefined;
+		return await this.userService.editProfile(user.id, fullname, avatar?.filename);
+	}
 }
