67c087845ee6b18971076fe982ccc44af5525238
Abouelwafa Choaib <cabouelw@student.1337.ma>
2022-09-01 14:56:52 +0100

    add channels to Global search

---
+++ client/components/search/SearchChannels.tsx
@@ -0,0 +1,61 @@
+import { motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import classes from "../../styles/Search.module.css";
+import { useOutsideAlerter } from "../Settings/ProfileInfoEdit";
+import Image from "next/image";
+import profile from "../../public/AvatarChannel.png";
+
+interface ChannelDataType {
+	Avatar: any;
+	fullName: string;
+	type: string;
+    isNew: boolean;
+}
+
+const Channel: React.FC<ChannelDataType> = (props) => {
+	return (
+		<div className={classes.Channel}>
+			<div className={classes.Avatar_Channel}>
+				<div className={classes.avatar}>
+					<Image src={props.Avatar} />
+				</div>
+				<div className={classes.ChannelName}>{props.fullName}</div>
+			</div>
+			<div className={classes.Channeltype}>{props.type}</div>
+			{props.isNew && <div className={classes.joinBtn}>
+				{/* <Image src='#' /> */}
+				Join
+			</div> || <div className={classes.LeaveBtn}>
+				{/* <Image src='#' /> */}
+				Exit
+			</div>}
+		</div>
+	);
+};
+
+const SearchChannelsList: React.FC = () => {
+	return (
+		<>
+			<motion.div className={classes.SearchCTNIN}>
+				<Channel fullName="One Piece" Avatar={profile} type="Public" isNew={false} />
+				<Channel
+					fullName="One Piece"
+					Avatar={profile}
+					type="Protected"
+                    isNew={true}
+				/>
+				<Channel fullName="One Piece" Avatar={profile} type="Public" isNew={true}/>
+				<Channel
+					fullName="One Piece"
+					Avatar={profile}
+					type="Protected"
+                    isNew={true}
+				/>
+				<Channel fullName="One Piece" Avatar={profile} type="Public" isNew={false}/>
+				<Channel fullName="One Piece" Avatar={profile} type="Public" isNew={true}/>
+			</motion.div>
+		</>
+	);
+};
+
+export default SearchChannelsList;
---
+++ client/components/search/search.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import classes from "../../styles/Search.module.css";
+import { motion } from "framer-motion";
+import SearchUserList from "./SearchUser";
+import SearchChannelsList from "./SearchChannels";
+
+const SearchComponent = () => {
+	const [overUsers, setOverUsers] = useState(true);
+
+	const showOverView = () => setOverUsers(true)
+
+	const hideOverView = () => setOverUsers(false)
+
+    let underLineOverView = overUsers ? classes.overViewbtn : " ";
+	let underLinefriends = !overUsers ? classes.FriendsBtn : " ";
+	let classesIndic = (!overUsers && classes.IndicChannel) || classes.Indic;
+	return (
+		<div className={classes.SearchCTN}>
+			<div className={classes.navView}>
+				<div
+					className={`${classes.TitleOverView}  ${underLineOverView}`}
+					onClick={showOverView}
+				>
+					Users
+				</div>
+				<div
+					className={`${classes.TitleOverView} ${underLinefriends} `}
+					onClick={hideOverView}
+				>
+					Channels
+				</div>
+			</div>
+            <div className={classes.ctnIndic}>
+				<motion.div
+					animate="animate"
+					className={classesIndic}
+				></motion.div>
+			</div>
+            {overUsers && <SearchUserList />}
+            {!overUsers && <SearchChannelsList  />}
+		</div>
+	);
+};
+
+export default SearchComponent;
