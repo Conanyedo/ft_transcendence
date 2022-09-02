f2e18b8d14dcbccfc976bd91e06dcf2763d9d57b
Abouelwafa Choaib <cabouelw@student.1337.ma>
2022-09-02 14:34:09 +0100

    remove server

--- server/src/2fa-jwt/2fa/2fa-auth.guard.ts
+++
@@ -1,5 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-@Injectable()
-export class Jwt2faAuthGuard extends AuthGuard('jwt-2fa') { }
--- server/src/auth/intra-auth.guard.ts
+++
@@ -1,5 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-@Injectable()
-export class IntraAuthGuard extends AuthGuard('42') { }
