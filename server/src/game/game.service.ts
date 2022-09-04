33e5673ebdc444ef3a8c56db7c8ed2fe5a6eec07
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-04 15:58:24 +0100

    add getMatches method based on login from db
    select matches that match login either in playerOne or playerTwo
    return an array of objects with needed info

--- server/src/game/game.service.ts
+++ server/src/game/game.service.ts
@@ -27,11 +27,19 @@ export class GameService {
 		const history: Game[] = await this.gameRepository
 			.createQueryBuilder('games')
 			.select(['games.playerOne', 'games.playerTwo', 'games.playerOneScore', 'games.playerTwoScore', 'games.date'])
-			.where(`games.playerOne = :login OR games.palyerTwo = :login`, { login: login })
+			.where(`games.playerOne = :login OR games.playerTwo = :login`, { login: login })
 			.getMany();
-		console.log(history);
-		// if (!history)
-		// 	throw new NotFoundException('User not found');
-		// return { ...user };
+		if (!history.length)
+			return history;
+		const matches = history.map((match) => {
+			const result = {
+				opponent: (match.playerOne === login) ? match.playerTwo : match.playerOne,
+				yourScore: (match.playerOne === login) ? match.playerOneScore : match.playerTwoScore,
+				opponentScore: (match.playerOne === login) ? match.playerTwoScore : match.playerOneScore,
+				data: match.date.toLocaleDateString("en-GB")
+			}
+			return result;
+		})
+		return matches;
 	}
 }
