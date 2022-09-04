b0f1479ae415fdedafebff99bb46651e3b329f51
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-04 19:38:15 +0100

    create FriendshipModule and entity

--- server/src/friendship/friendship.service.ts
+++ server/src/friendship/friendship.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Friendship } from './friendship.entity';
 
 @Injectable()
-export class FriendshipService {}
+export class FriendshipService {
+	constructor(
+		@InjectRepository(Friendship)
+		private friendshipRepository: Repository<Friendship>,
+	) { }
+}
