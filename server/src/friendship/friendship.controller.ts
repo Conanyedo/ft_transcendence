8675bf0035f5b18f1f3043160c1aec8142d900a5
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-05 00:28:47 +0100

    create routes to get friends, pending and blocked

--- server/src/friendship/friendship.controller.ts
+++ server/src/friendship/friendship.controller.ts
@@ -1,9 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Request, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/2fa-jwt/jwt/jwt-auth.guard';
+import { User } from 'src/user/user.decorator';
+import { userParitalDto } from 'src/user/user.dto';
 import { FriendshipService } from './friendship.service';
 
 @Controller('friendship')
 export class FriendshipController {
 	constructor(private readonly friendshipService: FriendshipService) {}
 
-	
+	@Get('/friends')
+	@UseGuards(JwtAuthGuard)
+	async getFriends(@User() user: userParitalDto) {
+		return await this.friendshipService.getFriends(user.login);
+	}
+
+	@Get('/pendings')
+	@UseGuards(JwtAuthGuard)
+	async getPendings(@User() user: userParitalDto) {
+		return await this.friendshipService.getPendings(user.login);
+	}
+
+	@Get('/blocked')
+	@UseGuards(JwtAuthGuard)
+	async getBlocked(@User() user: userParitalDto) {
+		return await this.friendshipService.getBlocked(user.login);
+	}
 }
