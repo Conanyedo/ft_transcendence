9a808acf9f4df800994e766041ab98b2da9e323c
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-07 16:53:36 +0100

    create notification entity to store notifs
    notif table has 'from' column which is the sender and 'to' column which
    is the receiver of the notif and the msg

---
+++ server/src/header/notification/notification.entity.ts
@@ -0,0 +1,21 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+export enum notifMsg {
+	INVITATION = 'Request',
+	GAME = 'Game',
+}
+
+@Entity({ name: 'notifications' })
+export class Notification {
+	@PrimaryGeneratedColumn("uuid")
+	id: string;
+
+	@Column()
+	from: string;
+
+	@Column()
+	to: string;
+
+	@Column({ type: 'enum', enum: notifMsg })
+	msg: notifMsg;
+}
--- server/src/header/notification/notification.module.ts
+++ server/src/header/notification/notification.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { NotificationGateway } from './notification.gateway';
+import { NotificationController } from './notification.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notification } from './notification.entity';
+import { NotificationService } from './notification.service';
 
 @Module({
-	providers: [NotificationGateway]
+	providers: [NotificationGateway, NotificationService],
+	controllers: [NotificationController],
+	imports: [TypeOrmModule.forFeature([Notification])]
 })
 export class NotificationModule {}
