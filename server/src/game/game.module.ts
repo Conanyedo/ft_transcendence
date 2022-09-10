8ff9cd44618fb941bff8b36c1b70baed73ce9e30
Abouelwafa Choaib <cabouelw@student.1337.ma>
2022-09-10 21:26:50 +0100

    add geteway to providers

--- server/src/game/game.module.ts
+++ server/src/game/game.module.ts
@@ -4,9 +4,10 @@ import { GameController } from './game.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Game } from './game.entity';
 import { UserModule } from 'src/user/user.module';
+import { GameGateway } from './game.gateway';
 
 @Module({
-  providers: [GameService],
+  providers: [GameService, GameGateway],
   controllers: [GameController],
   imports: [TypeOrmModule.forFeature([Game]), UserModule],
 })
