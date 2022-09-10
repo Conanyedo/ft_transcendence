ff71882cc17d3d0bf3cf8310af6c9e38b4895a18
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-10 22:06:29 +0100

    create webSockedGateway for notifications
    handle connection and disconnection
    listen on addFriend event to emit the notification to the right client

--- server/src/header/notification/notification.gateway.ts
+++ server/src/header/notification/notification.gateway.ts
@@ -1,28 +1,48 @@
-import { OnModuleInit } from "@nestjs/common";
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { Server } from 'socket.io'
+import { UseGuards } from "@nestjs/common";
+import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server, Socket } from 'socket.io'
 import { NotificationService } from "./notification.service";
 import { Notification, notifMsg } from './notification.entity';
+import { User } from "src/user/user.decorator";
+import { WsJwtGuard } from "src/2fa-jwt/jwt/jwt-ws.guard";
+
+class Online {
+	constructor(public socket: Socket, public login: string) {}
+}
 
 @WebSocketGateway()
-export class NotificationGateway implements OnModuleInit {
+export class NotificationGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+	constructor(private notifService: NotificationService) {}
 
 	@WebSocketServer()
 	server: Server;
 
-	onModuleInit() {
-		this.server.on('connection', (socket) => {
-			console.log('Connection : ', socket.id);
-		})
+	online: Online[] = [];
+
+	handleConnection(client: Socket) {
+		console.log('Connection : ', client.id);
+		const payload = this.notifService.connectClient(client);
+		if (payload)
+			this.online.push(new Online(client, payload.login));
 	}
 
-	@SubscribeMessage('addFriend')
-	addFriend(@MessageBody() data: Notification) {
-		console.log(data);
-		this.server.emit(`${data.to}`, {login: data.from, msg: data.msg}, (ack: string) => {
-			console.log('ack : ', ack);
-			if (!ack)
-				this.server.emit('storeNotif', data);
-		});
+	handleDisconnect(client: any) {
+		this.online = this.online.filter((user) => user.socket !== client)
+		console.log('Disconneted');
 	}
+
+	@UseGuards(WsJwtGuard)
+	@SubscribeMessage('addFriend')
+	addFriend(@User('login') login: string, @MessageBody() data: string) {
+		console.log('login: ', login);
+		console.log('friend: ', data);
+		// const client = this.online.find((el) => el.login === data).socket;
+		// client.emit('Notif', { login: login, msg: notifMsg.INVITATION });
+	}
+
+	// @SubscribeMessage('getNotifs')
+	// getNotifs(@MessageBody() data) {
+	// 	this.server.emit(`${data.to}`, { login: data.from, msg: data.msg });
+	// }
 }
