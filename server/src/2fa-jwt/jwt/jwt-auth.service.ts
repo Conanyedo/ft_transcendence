83b68cc6bafdb285c173d3c3a0d220ea957a072d
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-10 22:01:03 +0100

    create verify method to verify the passed token

--- server/src/2fa-jwt/jwt/jwt-auth.service.ts
+++ server/src/2fa-jwt/jwt/jwt-auth.service.ts
@@ -10,6 +10,10 @@ export class JwtAuthService {
 		private readonly configService: ConfigService
 	) { }
 
+	verify(token: string) {
+		return this.jwtService.verify(token, this.configService.get('JWT_SECRET_KEY'));
+	}
+
 	setJwt(user: userParitalDto) {
 		return this.jwtService.sign(user);
 	}
