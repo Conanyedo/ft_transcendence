79026e8a94f35e5be5b12759c6db8c93b18b55d3
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-10 22:02:54 +0100

    import jwtAuthModule to inject its service

--- server/src/header/notification/notification.module.ts
+++ server/src/header/notification/notification.module.ts
@@ -4,11 +4,12 @@ import { NotificationController } from './notification.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Notification } from './notification.entity';
 import { NotificationService } from './notification.service';
+import { JwtAuthModule } from 'src/2fa-jwt/jwt/jwt-auth.module';
 
 @Module({
 	providers: [NotificationGateway, NotificationService],
 	controllers: [NotificationController],
-	imports: [TypeOrmModule.forFeature([Notification])],
+	imports: [TypeOrmModule.forFeature([Notification]), JwtAuthModule],
 	exports: [NotificationService]
 })
 export class NotificationModule {}
