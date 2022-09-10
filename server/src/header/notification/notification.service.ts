ce622a7371b39a0d6ed74b0c366a0a45c9eb59ec
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-10 22:05:36 +0100

    create notificationService
    add connectClient method to check if client is authorized, otherwise
    disconnect it

--- server/src/header/notification/notification.service.ts
+++ server/src/header/notification/notification.service.ts
@@ -1,59 +1,37 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Notification, notifMsg } from './notification.entity';
-import { interval, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { switchMap } from 'rxjs/operators';
-import { io, Socket } from 'socket.io-client'
+import { Notification } from './notification.entity';
+import { Socket } from 'socket.io'
+import { JwtAuthService } from 'src/2fa-jwt/jwt/jwt-auth.service';
 
 @Injectable()
-export class NotificationService implements OnModuleInit {
-
-	private socket: Socket;
+export class NotificationService {
 
 	constructor(
 		@InjectRepository(Notification)
-		private notifRepository: Repository<Notification>
-	) {
-		this.socket = io('http://localhost:5000');
+		private notifRepository: Repository<Notification>,
+		private jwtService: JwtAuthService
+	) { }
+
+	connectClient(client: Socket) {
+		let token = client.handshake.headers.authorization;
+		token = token.split(' ')[1];
+		try {
+			const payload = this.jwtService.verify(token);
+			return (payload);
+		}
+		catch (e) {
+			client.disconnect();
+			return null;
+		}
 	}
 
-	onModuleInit() {
-		this.socket.on('connect', () => {
-			console.log('Connected to Gateway');
-		})
-	}
-
-	addFriendNotif(user: string, friend: string) {
-		this.socket.emit('addFriend', { from: user, to: friend, msg: notifMsg.INVITATION });
-		this.socket.on('storeNotif', (data: Notification) => {
-			const notif: Notification = new Notification();
-			notif.from = data.from;
-			notif.to = data.to;
-			notif.msg = data.msg;
-			this.notifRepository.save(notif);
-		})
-	}
-
-	// async getNotif(login: string): Promise<Observable<MessageEvent>> {
-	// 	let length = 0;
-	// 	return interval(1000).pipe(switchMap(async (index) => {
-	// 		const notifs: Notification[] = await this.notifRepository
-	// 			.createQueryBuilder('notifications')
-	// 			.select(['notifications.from', 'notifications.to', 'notifications.msg'])
-	// 			.where('notifications.to = :login', { login: login })
-	// 			.getMany();
-	// 		if (!notifs)
-	// 			return null;
-	// 		if (!notifs.length || notifs.length === length)
-	// 			return null;
-	// 		else {
-	// 			length = notifs.length;
-	// 			const msg = notifs.map((notif) => ({ login: notif.from, msg: notif.msg }))
-	// 			return ({ data: msg } as MessageEvent)
-	// 		}
-	// 	})
-	// 	);
+	// saveNofit(data: Notification) {
+	// 	const notif: Notification = new Notification();
+	// 	notif.from = data.from;
+	// 	notif.to = data.to;
+	// 	notif.msg = data.msg;
+	// 	this.notifRepository.save(notif);
 	// }
 }
