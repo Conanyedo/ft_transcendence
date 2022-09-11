c54954bd09d75b9ef29c87db7aa1e323236b6e38
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-11 14:58:02 +0100

    create setter and getter of socketId of the user

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -79,6 +79,15 @@ export class UserService {
 		};
 	}
 
+	async getSocketId(login: string) {
+		const user: User = await this.userRepository
+			.createQueryBuilder('users')
+			.select(['users.socketId'])
+			.where('users.login = :login', { login: login })
+			.getOne();
+		return user.socketId;
+	}
+
 	async getSecret(id: string) {
 		const user: User = await this.userRepository
 			.createQueryBuilder('users')
@@ -254,6 +263,14 @@ export class UserService {
 		return await this.userRepository
 			.createQueryBuilder('users')
 			.update({ status: status })
+			.where('id = :id', { id: id })
+			.execute();
+	}
+
+	async setSocketId(id: string, value: string | null) {
+		return await this.userRepository
+			.createQueryBuilder('users')
+			.update({ socketId: value })
 			.where('id = :id', { id: id })
 			.execute();
 	}
