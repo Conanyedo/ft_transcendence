988e7c94f29e354f7b32e2ddfd9dbd5b32142d25
Abouelwafa Choaib <cabouelw@student.1337.ma>
2022-09-15 23:17:38 +0100

    add list of Live Games & watch any game availebel

--- client/pages/live-games/index.tsx
+++ client/pages/live-games/index.tsx
@@ -1,14 +1,16 @@
+import { useEffect } from "react";
+import LiveGames from "../../components/liveGame/liveGame";
+import ContentWrapper from "../../components/wrapper/appWrapper";
+import socket_game from "../../config/socketGameConfig";
 
 const Game = () => {
 	return (
 		<>
-			<p
-				style={{
-					color: "white",
-				}}
-			>
-				Live Game
-			</p>
+			<ContentWrapper children={
+				<>
+				<LiveGames />
+				</>
+			}/>
 		</>
 	);
 };
