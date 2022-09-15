48f38a07901426ff3e5336324d53c575a738d0df
Abouelwafa Choaib <cabouelw@student.1337.ma>
2022-09-15 23:15:08 +0100

    remove the score from map and draw the map by width static from object Data

--- client/config/game.ts
+++ client/config/game.ts
@@ -1,11 +1,11 @@
 import { GameDataType } from "../Types/dataTypes";
 import { Theme } from "./gameMap";
 
-function drawRect(x: any, y: any, w: any, h: any, color: any, ctx: any) {
+function drawRect(x: number, y: number, w: number, h: number, color: string, ctx: CanvasRenderingContext2D) {
 	ctx.fillStyle = color;
 	ctx.fillRect(x, y, w, h);
 }
-function drawCircle(x: any, y: any, r: any, color: any, ctx: any) {
+function drawCircle(x: number, y: number, r: number, color: string, ctx: CanvasRenderingContext2D) {
 	ctx.fillStyle = color;
 	ctx.beginPath();
 	ctx.arc(x, y, r, 0, Math.PI * 2, false);
@@ -13,13 +13,13 @@ function drawCircle(x: any, y: any, r: any, color: any, ctx: any) {
 	ctx.fill();
 }
 
-function drawText(text: any, x: any, y: any, color: any, ctx: any) {
+function drawText(text: any, x: number, y: number, color: string, ctx: CanvasRenderingContext2D) {
 	ctx.fillStyle = color;
 	ctx.font = "5rem fantasy";
 	ctx.fillText(text, x, y);
 }
 
-function drawNet(data: GameDataType, ctx: any, lineColor: string) {
+function drawNet(data: GameDataType, ctx: CanvasRenderingContext2D, lineColor: string) {
 	for (let i = 0; i <= data.canvasHieght; i += 15) {
 		drawRect(data.canvasWidth / 2 - 2.5, i, 5, 10, lineColor, ctx);
 	}
@@ -27,24 +27,9 @@ function drawNet(data: GameDataType, ctx: any, lineColor: string) {
 
 export function render(data: GameDataType, canvas: any, theme: Theme) {
 	const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-	drawRect(0, 0, canvas.width, canvas.height, theme.backgroundColor, ctx);
+	drawRect(0, 0, data.canvasWidth, data.canvasHieght, theme.backgroundColor, ctx);
 
 	drawNet(data, ctx, theme.lineColor);
-
-	drawText(
-		data.myScore,
-		canvas.width / 4.5,
-		canvas.height / 5,
-		theme.textColor,
-		ctx
-	);
-	drawText(
-		data.otherScore,
-		(3 * canvas.width) / 4,
-		canvas.height / 5,
-		theme.textColor,
-		ctx
-	);
 
 	drawRect(
 		0,
