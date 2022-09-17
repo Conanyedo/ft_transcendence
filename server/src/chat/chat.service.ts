34b25c2bfcdc3837f00c48d9d875232abac66294
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-17 15:57:55 +0100

    add getConvbyId and createConv methods

--- server/src/chat/chat.service.ts
+++ server/src/chat/chat.service.ts
@@ -1,10 +1,10 @@
 import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Socket } from 'socket.io';
-import { JwtAuthService } from 'src/2fa-jwt/jwt/jwt-auth.service';
+import { Server, Socket } from 'socket.io';
 import { friendDto } from 'src/friendship/friendship.dto';
 import { UserService } from 'src/user/user.service';
 import { Repository } from 'typeorm';
+import { createMsgDto, msgDto } from './chat.dto';
 import { Conversation, Message } from './chat.entity';
 
 @Injectable()
@@ -15,7 +15,6 @@ export class ChatService {
 		@InjectRepository(Message)
 		private messageRepository: Repository<Message>,
 		private userService: UserService,
-		private jwtService: JwtAuthService,
 	) { }
 
 	async getConversations(login: string) {
@@ -34,12 +33,35 @@ export class ChatService {
 		return [...conversations];
 	}
 
+	async getConvById(convId: string) {
+		const conv: Conversation = await this.conversationRepository
+			.createQueryBuilder('conversations')
+			.where('conversations.id = :convId', { convId: convId })
+			.getOne();
+		return conv;
+	}
+
+	async updateConvDate(convId: string, date: Date) {
+		await this.conversationRepository
+			.createQueryBuilder('conversations')
+			.update({ lastUpdate: date })
+			.where('id = :convId', { convId: convId })
+			.execute();
+	}
+
 	async getMessages(convId: string) {
 		const msgs: Message[] = await this.messageRepository
 			.query(`SELECT messages."sender", messages."msg", messages."createDate" FROM messages where messages."conversationId" = '${convId}' order by messages."createDate" DESC;`)
 		if (!msgs.length)
 			return null;
 		return [...msgs];
+	}
+
+	async createConv(sender: string, receiver: string) {
+		const conv: Conversation = new Conversation();
+		conv.sender = sender;
+		conv.receiver = receiver;
+		return await this.conversationRepository.save(conv);
 	}
 
 	async storeMsg(msg: string, sender: string, convId: Conversation) {
@@ -51,15 +73,23 @@ export class ChatService {
 		return msgs.createDate;
 	}
 
-	async createConv(sender: string, receiver: string) {
-		console.log('Receiver: ', receiver);
-		let conv: Conversation = new Conversation();
-		conv.sender = sender;
-		conv.receiver = receiver;
-		return await this.conversationRepository.save(conv);
+	async createNewConv(server: Server, login: string, client: Socket, data: createMsgDto) {
+		const conv: Conversation = await this.createConv(login, data.receiver);
+		const date = await this.storeMsg(data.msg, login, conv);
+		client.join(conv.id);
+		const sockets = await server.fetchSockets();
+		const friendSocket = sockets.find((socket) => (socket.data.login === data.receiver))
+		if (friendSocket)
+			friendSocket.join(conv.id);
+		const msg: msgDto = { msg: data.msg, sender: login, date: date, convId: conv.id };
+		return msg;
 	}
 
-	async sendMsg(login: string, friend: string) {
-
+	async createNewMessage(login: string, data: createMsgDto) {
+		const conv = await this.getConvById(data.convId);
+		const date = await this.storeMsg(data.msg, login, conv);
+		this.updateConvDate(conv.id, date);
+		const msg: msgDto = { msg: data.msg, sender: login, date: date, convId: conv.id };
+		return msg;
 	}
 }
