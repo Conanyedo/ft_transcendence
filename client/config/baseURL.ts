3297829793be399efae2c59b710dc56b676d0561
Abouelwafa Choaib <cabouelw@student.1337.ma>
2022-09-18 18:37:03 +0100

    move the eraseCookie function to Functions file

--- client/config/baseURL.ts
+++ client/config/baseURL.ts
@@ -1,11 +1,3 @@
-import socket_notif from "./socketNotif";
-
 export const baseUrl: string = 'http://localhost:5000/';
-
-export function eraseCookie(name: string) {  
-    socket_notif.disconnect();
-    document.cookie = name+'=; Max-Age=0;';
-}
-
 
 export const allRanks = ['Gold', 'Silver', 'bronze', 'Jalouq', 'Unranked'];
