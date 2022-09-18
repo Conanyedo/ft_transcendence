4dc9f43532a3c46bd8fb3bc099f3ea6bcb693cbf
Abouelwafa Choaib <cabouelw@student.1337.ma>
2022-09-18 18:53:06 +0100

    create getters and setters

--- server/src/game/classes/Player.ts
+++ server/src/game/classes/Player.ts
@@ -3,16 +3,46 @@ import { GameControlers } from "./Game";
 import { Paddle } from "./Paddle";
 
 export class Player {
-	public login: string;
-	public socket: Socket;
-	public pos: string;
-	public paddle: Paddle;
-	public score: number;
+	private login: string;
+	private socket: Socket;
+	private pos: string;
+	private paddle: Paddle;
+	private score: number;
 	constructor(login: string, socket: Socket, pos: string) {
 		this.login = login;
 		this.socket = socket;
         this.pos = pos;
 		this.paddle = (pos !== 'left') ? new Paddle(GameControlers.canvasW - GameControlers.PaddleW) : new Paddle(0);
         this.score = 0;
+	}
+	getlogin() {
+		return this.login;
+	}
+	getsocket() {
+		return this.socket;
+	}
+	getpos() {
+		return this.pos;
+	}
+	getpaddle() {
+		return this.paddle;
+	}
+	getscore() {
+		return this.score;
+	}
+	setlogin(login: string) {
+		this.login = login;
+	}
+	setsocket(socket: Socket) {
+		this.socket = socket;
+	}
+	setpos(pos: string) {
+		this.pos = pos;
+	}
+	setpaddle(paddle: Paddle) {
+		this.paddle = paddle;
+	}
+	increasescore() {
+		this.score++;
 	}
 }
