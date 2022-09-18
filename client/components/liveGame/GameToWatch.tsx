445fc51b3db2efa5d827b40668796ec4ba0df1d2
Abouelwafa Choaib <cabouelw@student.1337.ma>
2022-09-18 18:44:23 +0100

    show the socket on to show the winner after the game End

--- client/components/liveGame/GameToWatch.tsx
+++ client/components/liveGame/GameToWatch.tsx
@@ -1,112 +1,47 @@
 import classes from "../../styles/liveGame.module.css";
-import Gold from "../../public/Tiers/Gold.svg";
-import Play from "../../public/WatchLiveGame.svg";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
-import { GameDataType, liveGamesType } from "../../Types/dataTypes";
+import { GameDataType } from "../../Types/dataTypes";
 import { useRouter } from "next/router";
 import { render } from "../../config/game";
-import { ThemeDarkMode, ThemeSky } from "../../config/gameMap";
+import { allTheme, ThemeDarkMode, ThemeSky } from "../../config/gameMap";
 import socket_game from "../../config/socketGameConfig";
+import { LiveGame } from "./liveGame";
+import WinerCard from "../game/WinerCard";
+import CanvasGame from "../game/CanvasGame";
 
-export const LiveGame: React.FC<liveGamesType> = (props) => {
-	const router = useRouter();
-	return (
-		<>
-			<div className={classes.liveGameContainer}>
-				<div className={classes.TypeGame}>{props.matchType}</div>
-				<div className={classes.GameContent}>
-					<div className={classes.UserSection}>
-						<div className={classes.userNameAvatar}>
-							<div className={classes.avatar}>
-								<img src="/uploads/cabouelw1662918372479x70.jpg" />
-							</div>
-							<div className={classes.userName}>
-								Choaib
-							</div>
-						</div>
-
-						<div className={classes.ScoreTier}>
-							<div className={classes.tierContainer}>
-								<Image src={Gold} />
-							</div>
-							<div className={classes.Score}>
-								{props.firstScore}
-							</div>
-						</div>
-					</div>
-					<div className={classes.vs}>VS</div>
-					<div className={classes.UserSectionSecond}>
-						<div className={classes.ScoreTier}>
-							<div className={classes.Score}>
-								{props.secondScore}
-							</div>
-							<div className={classes.tierContainerSecond}>
-								<Image src={Gold} />
-							</div>
-						</div>
-						<div className={classes.userNameAvatar}>
-							<div className={classes.secondUserName}>
-								{/* {props.secondPlayer} */}ikram
-							</div>
-							<div className={classes.secondavatar}>
-								<img src="/uploads/watchytb13371662644889624x70.jpg" />
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	);
-};
 
 const GameToWatch = () => {
 	const gameData: GameDataType = new GameDataType(1500);
-	const [newData, setNewData] = useState<GameDataType>(gameData);
+	const [staticData, setStaticData] = useState<GameDataType>(gameData);
+	const [showWinner, setShowWinner] = useState("");
 	const router = useRouter();
-	const ref_canvas = useRef(null);
 	useEffect(() => {
-		socket_game.emit("watchGame", { ID: router.query.id });
-		socket_game.on("gameStats", (data) => {
-			setNewData(data);
+		socket_game.on("GameOver", (data) => {
+			setShowWinner(data.winner);
+			setTimeout(() => {
+				router.replace("/live-games");
+			}, 4000);
+		});
+		socket_game.on("infoGame", (data) => {
+			setStaticData(data);
 		});
 		return () => {
 			socket_game.emit("exitFromSocket", { ID: router.query.id });
-			socket_game.off("gameStats");
+			socket_game.off("GameOver");
+			socket_game.off("infoGame");
 		};
 	}, []);
-	useEffect(() => {
-		gameData.ball.ballX = newData.ball.ballX;
-		gameData.ball.ballY = newData.ball.ballY;
-		gameData.myScore = newData.myScore;
-		gameData.otherScore = newData.otherScore;
-		gameData.paddleLeft.paddleY = newData.paddleLeft.paddleY;
-		gameData.paddleRight.paddleY = newData.paddleRight.paddleY;
-		gameData.GameStatus = newData.GameStatus;
-		render(
-			gameData,
-			ref_canvas.current,
-			Math.floor(Math.random() * 2) + 1 > 0 ? ThemeSky : ThemeDarkMode
-		);
-	}, [newData]);
 	return (
+		<div className={classes.container}>
 		<div className={classes.GameContainerOutside}>
+			{showWinner !== "" && <WinerCard showWinner={showWinner} />}
 			<LiveGame
-				firstPlayer="Choaib Abouelwafa"
-				secondPlayer="Ikram"
-				firstScore={newData.myScore}
-				secondScore={newData.otherScore}
-				matchType="classic"
-				gameId="s"
+				gameId={staticData.gameID}
+				click={false}
 			/>
-			<canvas
-				width={gameData.canvasWidth}
-				height={gameData.canvasHieght}
-				ref={ref_canvas}
-				id="pong"
-			></canvas>
-			ß
-		</div>
+			<CanvasGame GameID={staticData.gameID}/>
+		</div></div>
 	);
 };
 export default GameToWatch;
