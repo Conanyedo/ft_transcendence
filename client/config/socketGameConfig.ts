e29d57c2a1ea1df942a0844d528281999479e2cd
Abouelwafa Choaib <cabouelw@student.1337.ma>
2022-09-19 14:41:58 +0100

    get baseURL from env

--- client/config/socketGameConfig.ts
+++ client/config/socketGameConfig.ts
@@ -1,8 +1,9 @@
 import { getCookie } from "cookies-next";
 import { io } from "socket.io-client";
+import { baseUrlGame } from "./baseURL";
 
 const token = getCookie("jwt");
 
-const socket_game = io(`http://localhost:5551`, { query: { jwt: token } });
+const socket_game = io(`${baseUrlGame}`, { query: { jwt: token } });
 
 export default socket_game;
