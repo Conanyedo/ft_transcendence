8b7e00dd31498497eba5a8c270ca764707f83ab5
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-19 14:43:08 +0100

    add Member entity to reference members of a conv
    create ManyToOne relation with conversations table, many members can have the
    same convId
    create ManyToOne relation with users table, many memebers can reference
    the same userId
    add column for join/left for each member
    add type, avatar, name columns for conversations table

--- server/src/chat/chat.entity.ts
+++ server/src/chat/chat.entity.ts
@@ -1,21 +1,45 @@
+import { User } from "src/user/user.entity";
 import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+
+export enum memberStatus {
+	OWNER = "Owner",
+	ADMIN = "Admin",
+	MEMBER = "Member",
+}
+
+export enum convType {
+	DM = "Dm",
+	PUBLIC = "Public",
+	PROTECTED = "Protected",
+	PRIVATE = "Private",
+}
 
 @Entity({ name: 'conversations' })
 export class Conversation {
-	@PrimaryGeneratedColumn()
+	@PrimaryGeneratedColumn("uuid")
 	id: string;
 
-	@Column()
-	sender: string;
-
-	@Column()
-	receiver: string;
+	@Column({ default: null })
+	name: string;
 
 	@UpdateDateColumn()
 	lastUpdate: Date;
 
+	@Column({ type: 'enum', enum: convType, default: convType.DM })
+	type: convType;
+
+	@Column({ default: null })
+	password: string;
+
+	@Column({ default: null })
+	avatar: string;
+
 	@OneToMany(() => Message, (message) => message.conversation)
 	messages: Message[];
+
+	@OneToMany(() => Member, (member) => member.conversation)
+	members: Member[];
 }
 
 @Entity({ name: 'messages' })
@@ -34,5 +58,26 @@ export class Message {
 
 	@ManyToOne(() => Conversation, (conversation) => conversation.messages)
 	conversation: Conversation;
+}
+
+@Entity({ name: 'members' })
+export class Member {
+	@PrimaryGeneratedColumn("uuid")
+	id: string;
+
+	@Column({ type: 'enum', enum: memberStatus, default: memberStatus.MEMBER })
+	status: memberStatus;
+
+	@CreateDateColumn()
+	joinDate: Date;
+
+	@CreateDateColumn({ default: null })
+	leftDate: Date;
+
+	@ManyToOne(() => Conversation, (conversation) => conversation.members)
+	conversation: Conversation;
+
+	@ManyToOne(() => User, (user) => user.members)
+	user: User;
 }
 
