53b783a0be61f344e73b5f48d3d233f3696f0aa6
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-21 03:12:35 +0100

    update uploadUserConfig and and uploadChannelConfig
    uploadChannelConfig to upload channel avatar

--- server/src/config/upload.config.ts
+++ server/src/config/upload.config.ts
@@ -2,12 +2,22 @@ import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer
 import { diskStorage } from 'multer';
 import { userParitalDto } from 'src/user/user.dto';
 
-export const uploadConfig: MulterOptions = {
+export const uploadUsersConfig: MulterOptions = {
 	storage: diskStorage({
-		destination: '../client/public/uploads',
+		destination: '../client/public/uploads/users',
 		filename: (req, file, callback) => {
 			const user: userParitalDto = <userParitalDto>req.user;
 			const fileName = `${user.login + Date.now()}.jpg`
+			callback(null, fileName);
+		}
+	})
+}
+
+export const uploadChannelConfig: MulterOptions = {
+	storage: diskStorage({
+		destination: '../client/public/uploads/channels',
+		filename: (req, file, callback) => {
+			const fileName = `${req.body.convId + Date.now()}.jpg`
 			callback(null, fileName);
 		}
 	})
