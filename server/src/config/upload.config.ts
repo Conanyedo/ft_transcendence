0d381dfcf126101e03ed4bea8f98c64905856a53
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-21 15:26:53 +0100

    add fileFilter to multerOptions to filter files
    fileFilter store file if callback acceptFile property is true, otherwise
    ignore it
    create isFileValid function to check extension and mime type of a file
    from its stream and magic number, if invalid delete the uploaded file

--- server/src/config/upload.config.ts
+++ server/src/config/upload.config.ts
@@ -1,24 +1,79 @@
 import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 import { diskStorage } from 'multer';
 import { userParitalDto } from 'src/user/user.dto';
+import { fromStream } from 'file-type/core';
+
+import * as fs from 'fs';
+import * as path from 'path';
+import * as Jimp from 'jimp';
+
+const validFileExtensions: string[] = ['png', 'jpg', 'jpeg'];
+const validMimeTypes: string[] = ['image/png', 'image/jpg', 'image/jpeg'];
 
 export const uploadUsersConfig: MulterOptions = {
 	storage: diskStorage({
 		destination: '../client/public/uploads/users',
 		filename: (req, file, callback) => {
 			const user: userParitalDto = <userParitalDto>req.user;
-			const fileName = `${user.login + Date.now()}.jpg`
+			const ext = path.extname(file.originalname);
+			const fileName = `${user.login + Date.now()}` + ext;
 			callback(null, fileName);
 		}
-	})
+	}),
+	fileFilter: (req, file, callback) => {
+		if (!validMimeTypes.includes(file.mimetype))
+			callback(null, false);
+		else
+			callback(null, true);
+	}
 }
 
 export const uploadChannelConfig: MulterOptions = {
 	storage: diskStorage({
 		destination: '../client/public/uploads/channels',
 		filename: (req, file, callback) => {
-			const fileName = `${req.body.convId + Date.now()}.jpg`
+			const ext = path.extname(file.originalname);
+			const fileName = `${req.body.convId + Date.now()}` + ext;
 			callback(null, fileName);
-		}
-	})
+		},
+	}),
+	fileFilter: (req, file, callback) => {
+		if (!validMimeTypes.includes(file.mimetype))
+			callback(null, false);
+		else
+			callback(null, true);
+	}
+}
+
+export const isFileValid = async (uploadPath: string, name: string) => {
+	const stream = fs.createReadStream(`../client/public/uploads/${uploadPath}/${name}`);
+	const fileType = await fromStream(stream);
+	const isValid: boolean = validFileExtensions.includes(fileType.ext) && validMimeTypes.includes(fileType.mime);
+	if (!isValid) {
+		fs.unlink(`../client/public/uploads/${uploadPath}/${name}`, (err) => { });
+		return undefined;
+	}
+	return name;
+}
+
+export const deleteAvatar = async (uploadPath: string, name: string) => {
+	if (name)
+		fs.unlink(`../client/public/uploads/${uploadPath}/${name}`, (err) => { });
+}
+
+export const deleteOldAvatar = async (uploadPath: string, name: string) => {
+	const ext = path.extname(name);
+	let oldName = name.split('/').pop();
+	oldName = oldName.slice(0, oldName.indexOf(ext));
+	deleteAvatar(uploadPath, `${oldName}x70${ext}`);
+	deleteAvatar(uploadPath, `${oldName}x220${ext}`);
+}
+
+export const resizeAvatar = async (uploadPath: string, name: string) => {
+	const ext = path.extname(name);
+	const image = await Jimp.read(`../client/public/uploads/${uploadPath}/${name}`);
+	const resizeName = name.slice(0, name.indexOf(ext));
+	image.resize(220, 220).write(`../client/public/uploads/${uploadPath}/${resizeName}x220${ext}`);
+	image.resize(70, 70).write(`../client/public/uploads/${uploadPath}/${resizeName}x70${ext}`);
+	deleteAvatar(uploadPath, name);
 }
