774f6fefbd83d146e5b53f3f7de14ccd2da3a321
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-23 04:58:52 +0100

    create banMembers route for banning members

--- server/src/chat/chat.controller.ts
+++ server/src/chat/chat.controller.ts
@@ -55,6 +55,12 @@ export class ChatController {
 		return await this.chatService.addMembers(login, convId, members);
 	}
 
+	@Post('/banMember')
+	@UseGuards(JwtAuthGuard)
+	async banMember(@User('login') login: string, @Body('convId') convId: string, @Body('member') member: string) {
+		return await this.chatService.banMember(login, convId, member);
+	}
+
 	@Post('/updateChannel')
 	@UseGuards(JwtAuthGuard)
 	@UseInterceptors(FileInterceptor('avatar', uploadChannelConfig))
