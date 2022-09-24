4cab25aa2efa3b6181812d0ddc0e4bdc6313eeb2
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-24 11:44:08 +0100

    import friendshipModule to use its services

--- server/src/chat/chat.module.ts
+++ server/src/chat/chat.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,11 +6,12 @@ import { JwtAuthModule } from 'src/2fa-jwt/jwt/jwt-auth.module';
 import { UserModule } from 'src/user/user.module';
 import { Conversation, Member, Message } from './chat.entity';
 import { ChatGateway } from './chat.gateway';
+import { FriendshipModule } from 'src/friendship/friendship.module';
 
 @Module({
   providers: [ChatService, ChatGateway],
   controllers: [ChatController],
-	imports: [TypeOrmModule.forFeature([Message, Conversation, Member]), JwtAuthModule, UserModule],
+	imports: [TypeOrmModule.forFeature([Message, Conversation, Member]), JwtAuthModule, forwardRef(() =>UserModule), forwardRef(() => FriendshipModule)],
 	exports: [ChatService]
 })
 export class ChatModule {}
