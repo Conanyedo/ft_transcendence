f8c3fe81cabd27e958e88b0fc30765ce25a3733b
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-09-28 17:57:01 +0100

    update players stats after a pong game
    update winner and loser stats, rank and achievements depending on type of match after each match, update all users ranks

--- server/src/game/game.service.ts
+++ server/src/game/game.service.ts
@@ -1,6 +1,9 @@
 import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { stat } from 'fs';
 import { FriendshipService } from 'src/friendship/friendship.service';
+import { Stats, userAchievements } from 'src/user/stats.entity';
+import { rankDto, statsDto } from 'src/user/user.dto';
 import { UserService } from 'src/user/user.service';
 import { Repository } from 'typeorm';
 import { gameDto, historyDto, opponentDto } from './game.dto';
@@ -11,9 +14,62 @@ export class GameService {
 	constructor(
 		@InjectRepository(Game)
 		private gameRepository: Repository<Game>,
-		private readonly userService: UserService,
-		private readonly friendshipService: FriendshipService
+		private readonly userService: UserService
 	) { }
+
+	async getRecentMatchesWon(player: string) {
+		const matches = await this.gameRepository
+			.query(`select games."playerOne", games."playerTwo", games."playerOneScore", games."playerTwoScore", games.date from games where (games."playerOne" = '${player}' or games."playerTwo" = '${player}') order by games.date DESC limit 20`);
+		if (matches.length >= 10) {
+			const loss = matches.find((match, idx) => {
+				if ((match.playerOne === player && match.playerOneScore < match.playerTwoScore) ||
+					(match.playerTwo === player && match.playerOneScore > match.playerTwoScore))
+					return { ...match, idx };
+			});
+			if (!loss && matches.length === 20)
+				return 20;
+			if (!loss || (loss && loss.idx >= 10))
+				return 10;
+		}
+		return 0;
+	}
+
+
+	async updateStats(player: string, gameType: string, winner: boolean) {
+		const stats: statsDto = await this.userService.getStats(player);
+		stats.numGames++;
+		stats.gamesWon += (winner) ? 1 : 0;
+		if (gameType === 'Ranked')
+			stats.GP += (winner) ? 50 : (stats.GP >= 25) ? -25 : 0;
+		stats.XP += (1000) / (Math.floor(stats.XP / 1000) ^ 2);
+		await this.userService.updateStats(stats);
+		const rankList: rankDto[] = await this.userService.getRank();
+		const toBeUpdated: rankDto[] = rankList.filter((rank, index) => {
+			if (rank.rank !== index + 1) {
+				rank.rank = index + 1;
+				return rank;
+			}
+		});
+		toBeUpdated.forEach((rank) => (this.userService.updateRank(rank.id, rank.rank)))
+
+		if (!stats.achievement.includes(userAchievements.FIRSTWIN) && winner && (gameType === 'Ranked'))
+			stats.achievement.push(userAchievements.FIRSTWIN);
+		if (stats.gamesWon >= 10 && winner) {
+			const achiev: number = await this.getRecentMatchesWon(player);
+			if (achiev === 10 && !stats.achievement.includes(userAchievements.WON10))
+				stats.achievement.push(userAchievements.WON10);
+			if (achiev === 20 && !stats.achievement.includes(userAchievements.WON20))
+				stats.achievement.push(userAchievements.WON20);
+		}
+		if (stats.XP >= 5000 && !stats.achievement.includes(userAchievements.LEVEL5))
+			stats.achievement.push(userAchievements.LEVEL5);
+		if (stats.GP >= 1600 && !stats.achievement.includes(userAchievements.GOLDTIER) && winner)
+			stats.achievement.push(userAchievements.GOLDTIER);
+		stats.rank = toBeUpdated.find((rank) => (rank.id === stats.id))?.rank;
+		if (stats.rank === 1 && !stats.achievement.includes(userAchievements.FIRSTPLACE) && winner)
+			stats.achievement.push(userAchievements.FIRSTPLACE);
+		await this.userService.updateAchievements(stats.id, stats.achievement);
+	}
 
 	async insertMatches(gameResult: gameDto) {
 		let game: Game = new Game();
@@ -24,9 +80,8 @@ export class GameService {
 		game = await this.gameRepository.save(game);
 		const winner: string = (game.playerOneScore > game.playerTwoScore) ? game.playerOne : game.playerTwo;
 		const loser: string = (winner === game.playerOne) ? game.playerTwo : game.playerOne;
-		// this.userService.updateWinnerStats(winner);
-		// this.userService.updateLoserStats(loser);
-		// console.log('Game Result : ', game);
+		await this.updateStats(winner, gameResult.gameType, true);
+		await this.updateStats(loser, gameResult.gameType, false);
 	}
 
 	async getMatches(login: string) {
@@ -34,6 +89,7 @@ export class GameService {
 			.createQueryBuilder('games')
 			.select(['games.playerOne', 'games.playerTwo', 'games.playerOneScore', 'games.playerTwoScore', 'games.date'])
 			.where(`games.playerOne = :login OR games.playerTwo = :login`, { login: login })
+			.orderBy("games.date", "DESC")
 			.getMany();
 		if (!history.length)
 			return history;
