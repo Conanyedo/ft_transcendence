a0170b2db49faeae632573be8eedc4d1b6ed06b1
Abouelwafa Choaib <cabouelw@student.1337.ma>
2022-10-02 22:39:29 +0100

    send token to connect with game Socket

--- client/config/socketGameConfig.ts
+++ client/config/socketGameConfig.ts
@@ -3,7 +3,16 @@ import { io } from "socket.io-client";
 import { baseUrlGame } from "./baseURL";
 
 const token = getCookie("jwt");
+const socketOptions = {
+    transportOptions: {
+      polling: {
+        extraHeaders: {
+          Authorization: `Bearer ${token}`,
+        }
+      }
+    }
+ };
 
-const socket_game = io(`${baseUrlGame}`, { query: { jwt: token } });
+const socket_game = io(`${baseUrlGame}`, socketOptions);
 
 export default socket_game;
