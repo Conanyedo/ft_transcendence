eb5cc034f69298709c6c2f43b68448e6f17e1541
Abouelwafa Choaib <cabouelw@student.1337.ma>
2022-10-06 00:33:37 +0100

    add logique to show/hide error message

--- client/store/UI-Slice.tsx
+++ client/store/UI-Slice.tsx
@@ -5,13 +5,15 @@ import classes from "../../styles/sideNav.module.css";
 export type CounterState = {
 	updateCart: boolean;
 	showError: boolean;
+	showErrorGame: boolean;
 	showSetting: boolean;
 };
 
 const initialState: CounterState = {
 	updateCart: false,
 	showSetting: false,
-	showError: false
+	showError: false,
+	showErrorGame: false
 };
 
 export const UI_Slice = createSlice({
@@ -27,6 +29,12 @@ export const UI_Slice = createSlice({
 		HideErrorMsg: (state) => {
 			state.showError = false;
 		},
+		ShowErrorGameMsg: (state) => {
+			state.showErrorGame = true;
+		},
+		HideErrorGameMsg: (state) => {
+			state.showErrorGame = false;
+		},
 		ShowSettings: (state) => {
 			state.showSetting = true;
 		},
@@ -35,9 +43,10 @@ export const UI_Slice = createSlice({
 		}
 	},
 });
-export const { Toggle, ShowErrorMsg, HideErrorMsg, ShowSettings, HideSettings } = UI_Slice.actions;
+export const { Toggle, ShowErrorMsg, HideErrorMsg, ShowSettings, HideSettings, HideErrorGameMsg, ShowErrorGameMsg } = UI_Slice.actions;
 export const ToggleValue = (state: RootState) => state.UI.updateCart;
 export const ToggleErrorValue = (state: RootState) => state.UI.showError;
+export const ToggleErrorGameValue = (state: RootState) => state.UI.showErrorGame;
 export const Settings = (state: RootState) => state.UI.showSetting;
 
 export default UI_Slice.reducer;
