58e5f692e66fee8d72928246aea13161760851b7
Abouelwafa Choaib <cabouelw@student.1337.ma>
2022-10-06 00:24:12 +0100

    add logique to the Game invitation Card

--- client/components/Chat/inviteMsg.tsx
+++ client/components/Chat/inviteMsg.tsx
@@ -1,6 +1,5 @@
-import { chatUser, EmtyUser, UserTypeNew } from "@Types/dataTypes";
+import { EmtyUser, UserTypeNew } from "@Types/dataTypes";
 import Styles from "@styles/chat.module.css";
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import { fetchDATA } from "@hooks/useFetchData";
 import Router from "next/router";
@@ -8,33 +7,48 @@ import { getImageBySize } from "@hooks/Functions";
 import socket_game from "config/socketGameConfig";
 import socket_notif from "config/socketNotif";
 
-export function InviteMsg(props: {senderLogin: string, GameID: string}) {
+export function InviteMsg(props: {chatMsg: any}) {
   const [userInfo, setuserInfo] = useState<UserTypeNew>(EmtyUser);
+  const [msgData, setmsgData] = useState<any>();
   const me = localStorage.getItem("owner");
-
+  useEffect(() => {
+    setmsgData(props.chatMsg);
+  }, [props.chatMsg])
+  
   const acceptHandler = async () => {
     socket_game.emit(
       "joinGameFriend",
-      { login: me, accept: props.GameID },
+      { login: me, accept: msgData?.invitation },
       (data: string) => {
         if (data.length > 4) {
+          socket_notif.emit('updateInvitation', {convId: msgData?.convId, msgId: msgData?.msgId, status: 'Accepted' }, (res: any) => {
+            setmsgData(res.data);
+          })
           Router.push("/game/lobby?gameID=" + data);
         }
       }
     );
   };
   const refuseHandler = async () => {
-    socket_game.emit("refuseChallenge", props.GameID);
+    socket_game.emit("refuseChallenge", msgData?.invitation);
+    socket_notif.emit('updateInvitation', {convId: msgData?.convId, msgId: msgData?.msgId, status: 'Refused' }, (res: any) => {
+      setmsgData(res.data);
+    })
   };
   const cancelHandler = () => {
-    socket_game.emit("removeGameLobby", props.GameID);
+    socket_game.emit("removeGameLobby", msgData?.invitation);
+    socket_notif.emit('updateInvitation', {convId: msgData?.convId, msgId: msgData?.msgId, status: 'Canceled' }, (res: any) => {
+      setmsgData(res.data);
+    })
   };
   useEffect(() => {
-    fetchDATA(setuserInfo, Router, "user/info/" + props.senderLogin);
-  }, []);
-
+    if (msgData?.sender !== '')
+      fetchDATA(setuserInfo, Router, "user/info/" + msgData?.sender);
+  }, [msgData?.sender]);
+  
   const pathAvatar = getImageBySize(userInfo?.avatar, 70);
-  if (props.senderLogin == me)
+  console.log(msgData);
+  if (msgData?.sender == me)
     return (
       <div className={Styles.inviteMsg}>
         <div className={Styles.inviteCont}>
@@ -46,12 +60,15 @@ export function InviteMsg(props: {senderLogin: string, GameID: string}) {
           />
           <div className={Styles.inviteUsr}>
             {userInfo?.fullname}
-            <p>You invite them to play pong game</p>
+            {msgData?.status === 'Sent' && <p>You invited them to play pong game</p>}
+            {msgData?.status === 'Canceled' && <p>the invitation has been canceled</p>}
+            {msgData?.status === 'Refused' && <p>the invitation has been Refused</p>}
+            {msgData?.status === 'Accepted' && <p>the invitation has been Accepted</p>}
           </div>
         </div>
-        <button className={Styles.inviteBtn} onClick={cancelHandler}>
+        {msgData?.status === 'Sent' && <button className={Styles.inviteBtn} onClick={cancelHandler}>
           Cancel Invitation
-        </button>
+        </button>}
       </div>
     );
   return (
@@ -67,17 +84,20 @@ export function InviteMsg(props: {senderLogin: string, GameID: string}) {
         </div>
         <div className={Styles.inviteTxt}>
           {userInfo?.fullname}
-          <p>Invites you to play pong game</p>
+            {msgData?.status === 'Sent' && <p>You invited them to play pong game</p>}
+            {msgData?.status === 'Canceled' && <p>the invitation has been canceled</p>}
+            {msgData?.status === 'Refused' && <p>You refused the invitation</p>}
+            {msgData?.status === 'Accepted' && <p>You accepted the invitation</p>}
         </div>
       </div>
-      <div className={Styles.inviteBtns}>
+      {msgData?.status === 'Sent' && <div className={Styles.inviteBtns}>
         <button className={Styles.inviteBtn} onClick={acceptHandler}>
           Accept
         </button>
         <button className={Styles.inviteBtn} onClick={refuseHandler}>
           Refuse
         </button>
-      </div>
+      </div>}
     </div>
   );
 }
