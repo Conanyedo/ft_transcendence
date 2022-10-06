0fff42c6d30fb3ff2dc363ba4e4e2c1a4ce7af68
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-10-06 22:41:12 +0100

    create searchValidate to validate data in query

---
+++ server/src/header/search/search.dto.ts
@@ -0,0 +1,10 @@
+import { IsEnum, IsNotEmpty, IsString, IsUUID, Length, Matches } from "class-validator";
+
+export class searchValidate {
+	@IsNotEmpty()
+	@IsString()
+	@Length(2, 20)
+	@Matches(/^(?=.{2,20}$)(?![ _.-])(?!.*[_.-]{2})[a-zA-Z0-9 ._-]+(?<![ _.-])$/)
+	search: string;
+
+}
