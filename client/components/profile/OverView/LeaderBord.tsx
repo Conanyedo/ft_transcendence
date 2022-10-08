63ff34aad0d7bd53297d1f6ea2999bbb3e96cc77
Abouelwafa Choaib <cabouelw@student.1337.ma>
2022-10-08 04:20:20 +0100

    update: fix leaks in useEffect

--- client/components/profile/OverView/LeaderBord.tsx
+++ client/components/profile/OverView/LeaderBord.tsx
@@ -1,63 +1,51 @@
-import classes from "../../../styles/overView.module.css";
-import Rank_1 from "../../../public/LeaderBoard/FirstPlace.svg";
-import Rank_2 from "../../../public/LeaderBoard/Second.svg";
-import Rank_3 from "../../../public/LeaderBoard/thirdPlace.svg";
-import Image from "next/image";
-import { isMobile } from "react-device-detect";
-import { useEffect, useState } from "react";
-import { EmtyUser, matchDataType, rankObj, UserTypeNew } from "../../../Types/dataTypes";
-import Chart from "../../chart/chartProgress";
-import { motion } from "framer-motion";
-import { useRouter } from "next/router";
-import { fetchDATA } from "../../../customHooks/useFetchData";
-import { getImageBySize, getRankUser } from "../../../customHooks/Functions";
+import classes from "../../../styles/overView.module.css"
+import Rank_1 from "../../../public/LeaderBoard/FirstPlace.svg"
+import Rank_2 from "../../../public/LeaderBoard/Second.svg"
+import Rank_3 from "../../../public/LeaderBoard/thirdPlace.svg"
+import Image from "next/image"
+import { isMobile } from "react-device-detect"
+import { useEffect, useState } from "react"
+import { EmtyUser, matchDataType, rankObj, UserTypeNew } from "../../../Types/dataTypes"
+import Chart from "../../chart/chartProgress"
+import { motion } from "framer-motion"
+import { useRouter } from "next/router"
+import { fetchDATA } from "../../../customHooks/useFetchData"
+import { getImageBySize, getRankUser } from "../../../customHooks/Functions"
 
 const ElmRank: React.FC<matchDataType> = (props) => {
-	const avatar =
-		props.badge === 1
-			? Rank_1
-			: props.badge === 2
-			? Rank_2
-			: props.badge === 3
-			? Rank_3
-			: null;
-	const route = useRouter();
+	const avatar = props.badge === 1 ? Rank_1 : props.badge === 2 ? Rank_2 : props.badge === 3 ? Rank_3 : null
+	const route = useRouter()
 	const Clickhandler = () => {
-		if (props.relation !== "blocked") route.push("/profile/" + props.login);
-	};
-	const tier: rankObj = getRankUser(props.GP);
-	let winRatio = Math.floor((props.Win / props.games) * 100);
-	winRatio = (winRatio) ? winRatio : 0;
-	const pathImage = getImageBySize(props.avatar, 70);
+		if (props.relation !== "blocked") route.push("/profile/" + props.login)
+	}
+	const tier: rankObj = getRankUser(props.GP)
+	let winRatio = Math.floor((props.Win / props.games) * 100)
+	winRatio = winRatio ? winRatio : 0
+	const pathImage = getImageBySize(props.avatar, 70)
 	return (
 		<div className={classes.tableElments}>
 			<div className={classes.Rank}>
 				<div className={classes.SvgSize}>
-					{(props.badge > 3 && props.badge) ||
-						(avatar && <Image src={avatar} />)}
+					{(props.badge > 3 && props.badge) || (avatar && <Image src={avatar} />)}
 				</div>
 			</div>
-			<div
-				className={`${classes.User} ${classes.UserTR}`}
-				onClick={Clickhandler}
-			>
+			<div className={`${classes.User} ${classes.UserTR}`} onClick={Clickhandler}>
 				<div className={classes.profileUser}>
 					<img src={pathImage} />
 				</div>
 				{props.fullName}
 			</div>
-			<div className={`${classes.games} ${classes.gamesTotal}`}>
-				{props.games}
-			</div>
+			<div className={`${classes.games} ${classes.gamesTotal}`}>{props.games}</div>
 			<div className={classes.WinRatio}>
-				<div className={classes.WinRatioCalc}>{`${winRatio} % - ${props.Win}W / ${props.games - props.Win}L`}</div>
+				<div className={classes.WinRatioCalc}>{`${winRatio} % - ${props.Win}W / ${
+					props.games - props.Win
+				}L`}</div>
 				<div className={classes.pourcentagectn}>
 					<div
 						className={classes.pourcentage}
 						style={{
 							width: `${winRatio}%`,
-						}}
-					></div>
+						}}></div>
 				</div>
 			</div>
 			<div className={classes.Tier}>
@@ -66,108 +54,80 @@ const ElmRank: React.FC<matchDataType> = (props) => {
 				</div>
 			</div>
 		</div>
-	);
-};
+	)
+}
 
 const Stats: React.FC = () => {
-	const [user, setUser] = useState<UserTypeNew>(EmtyUser);
-	const router = useRouter();
-	const login = router.query?.id;
+	const [user, setUser] = useState<UserTypeNew>(EmtyUser)
+	const [isUp, SetIsUp] = useState(false)
+	const router = useRouter()
+	const login = router.query?.id
 	useEffect(() => {
-		fetchDATA(setUser, router, `user/stats${login ? "/" + login : ""}`);
-	}, [login]);
-	const amount =
-		user?.gamesWon === 0
-			? 0
-			: Math.floor((user?.gamesWon / user?.numGames) * 100);
+		fetchDATA(setUser, router, `user/stats${login ? "/" + login : ""}`)
+		SetIsUp(true)
+		return () => {
+			setUser(EmtyUser);
+		}
+	}, [login])
+	const amount = user?.gamesWon === 0 ? 0 : Math.floor((user?.gamesWon / user?.numGames) * 100)
 	return (
-		<div className={classes.stat}>
-			<div className={classes.LeaderBordTitle}>Stats</div>
-			<div className={classes.statsctn}>
-				<div className={classes.statDig}>
-					<div className={classes.WinRateTitle}>Win Rate</div>
-					<div className={classes.imageCTN}>
-						{user && <Chart amount={amount} />}
+		<>
+			{isUp && (
+				<div className={classes.stat}>
+					<div className={classes.LeaderBordTitle}>Stats</div>
+					<div className={classes.statsctn}>
+						<div className={classes.statDig}>
+							<div className={classes.WinRateTitle}>Win Rate</div>
+							<div className={classes.imageCTN}>{user && <Chart amount={amount} />}</div>
+						</div>
+						<div className={classes.statbtns}>
+							{user && (
+								<>
+									<div className={classes.cntBTNstat}>{user?.numGames} games</div>
+									<div className={classes.cntBTNstat}>{user?.gamesWon} Wins</div>
+									<div className={classes.cntBTNstat}>{user?.numGames - user?.gamesWon} Losses</div>
+								</>
+							)}
+						</div>
 					</div>
 				</div>
-				<div className={classes.statbtns}>
-					{user && (
-						<>
-							<div className={classes.cntBTNstat}>
-								{user?.numGames} games
-							</div>
-							<div className={classes.cntBTNstat}>
-								{user?.gamesWon} Wins
-							</div>
-							<div className={classes.cntBTNstat}>
-								{user?.numGames - user?.gamesWon} Losses
-							</div>
-						</>
-					)}
-				</div>
-			</div>
-		</div>
-	);
-};
+			)}
+		</>
+	)
+}
 
 const LeaderBoard: React.FC = () => {
-	const [listUsers, setListUsers] = useState<UserTypeNew[]>([]);
-	const [isUp, SetIsUp] = useState(false);
-	const router = useRouter();
+	const [listUsers, setListUsers] = useState<UserTypeNew[]>([])
+	const [isUp, SetIsUp] = useState(false)
+	const router = useRouter()
 	useEffect(() => {
-		fetchDATA(setListUsers, router, "user/leaderborad");
-		SetIsUp(true);
-	}, []);
+		fetchDATA(setListUsers, router, "user/leaderborad")
+		SetIsUp(true)
+		return () => {
+			setListUsers([])
+		}
+	}, [])
 
 	return (
 		<>
 			{isUp && (
 				<div className={classes.leaderBoard}>
 					<div className={classes.table}>
-						<div className={classes.LeaderBordTitle}>
-							LeaderBoard
-						</div>
+						<div className={classes.LeaderBordTitle}>LeaderBoard</div>
 						<div className={classes.leaderBoardctn}>
-							<div
-								className={`${
-									isMobile
-										? classes.tableTitlesInMobile
-										: classes.tableTitles
-								}`}
-							>
-								<div
-									className={`${classes.Rank} ${classes.tableTitle}`}
-								>
-									Rank
-								</div>
-								<div
-									className={`${classes.User} ${classes.tableTitle}`}
-								>
-									Users
-								</div>
-								<div
-									className={`${classes.games} ${classes.tableTitle}`}
-								>
-									Games
-								</div>
-								<div
-									className={`${classes.WinRatio} ${classes.tableTitle}`}
-								>
-									Win Ratio
-								</div>
-								<div
-									className={`${classes.Tier} ${classes.tableTitle}`}
-								>
-									Tier
-								</div>
+							<div className={`${isMobile ? classes.tableTitlesInMobile : classes.tableTitles}`}>
+								<div className={`${classes.Rank} ${classes.tableTitle}`}>Rank</div>
+								<div className={`${classes.User} ${classes.tableTitle}`}>Users</div>
+								<div className={`${classes.games} ${classes.tableTitle}`}>Games</div>
+								<div className={`${classes.WinRatio} ${classes.tableTitle}`}>Win Ratio</div>
+								<div className={`${classes.Tier} ${classes.tableTitle}`}>Tier</div>
 							</div>
 							<motion.div
 								initial={{ y: 10, opacity: 0 }}
 								animate={{ y: 0, opacity: 1 }}
 								exit={{ y: -10, opacity: 0 }}
 								transition={{ duration: 0.5 }}
-								className={classes.leaderBoardTable}
-							>
+								className={classes.leaderBoardTable}>
 								{listUsers &&
 									listUsers.map((user, idx) => (
 										<ElmRank
@@ -189,7 +149,7 @@ const LeaderBoard: React.FC = () => {
 				</div>
 			)}
 		</>
-	);
-};
+	)
+}
 
-export default LeaderBoard;
+export default LeaderBoard
