25b90d48b2c552daef60c0a73c447585a586fad7
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2022-12-12 22:51:35 +0100

    extract jwt and jwt-2fa from cookies

--- server/src/2fa-jwt/2fa/2fa-auth.strategy.ts
+++ server/src/2fa-jwt/2fa/2fa-auth.strategy.ts
@@ -2,14 +2,14 @@ import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { ConfigService } from '@nestjs/config';
-import { UserService } from "src/user/user.service";
-import { userDto, userParitalDto } from "src/user/user.dto";
+import { userParitalDto } from "src/user/user.dto";
+import { Request } from "express";
 
 @Injectable()
 export class Jwt2faAuthStrategy extends PassportStrategy(Strategy, 'jwt-2fa') {
 	constructor(private readonly configService: ConfigService) {
 		super({
-			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			jwtFromRequest: ExtractJwt.fromExtractors([Jwt2faAuthStrategy.extractJwt]),
 			ignoreExpiration: false,
 			secretOrKey: configService.get('2FA_JWT_SECRET_KEY'),
 		});
@@ -18,5 +18,11 @@ export class Jwt2faAuthStrategy extends PassportStrategy(Strategy, 'jwt-2fa') {
 	async validate(payload: userParitalDto) {
 		const user: userParitalDto = { id: payload.id, login: payload.login };
 		return user;
+	}
+
+	private static extractJwt(req: Request) {
+		if (req.cookies && req.cookies['jwt-2fa'] && req.cookies['jwt-2fa'].length)
+			return req.cookies.jwt;
+		return null;
 	}
 }
