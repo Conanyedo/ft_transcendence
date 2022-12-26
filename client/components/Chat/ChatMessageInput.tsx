4085f721f0bbabfc2efb19e3a4c6bd1da82e7a8f
0xOd3n <abdellahbelhachmi13915@gmail.com>
2022-12-27 00:06:20 +0100

    modify : adjust hide / show SendMsg icon

--- client/components/Chat/ChatMessageInput.tsx
+++ client/components/Chat/ChatMessageInput.tsx
@@ -1,35 +1,39 @@
-import Styles from '@styles/Chat/ChatMessageInput.module.css'
-import SendMsgIcon from '@public/Chat/send-arrow.svg';
-import InviteGameicon from '@public/Chat/Gamepad.svg';
-import { useState } from 'react';
+import Styles from "@styles/Chat/ChatMessageInput.module.css";
+import SendMsgIcon from "@public/Chat/send-arrow.svg";
+import InviteGameicon from "@public/Chat/Gamepad.svg";
+import { useState } from "react";
+import { string } from "yup";
 
 export const ChatMessageInput = () => {
-    const [isTyping, setisTyping] = useState(false);
+  const [EnteredMsg, setEnteredMsg] = useState<string>("");
 
-    const isTypingHandler = () => {
-        setisTyping(true);
-    };
-
-    if (isTyping)
-        return (
-            <>
-            <div className={Styles.ChatMsgSendBoxContainer}>
-                <div className={Styles.ChatMsgSendBox}>
-                    <input type={'text'} placeholder='Message'></input>
-                    <img src={InviteGameicon.src} alt='SendMsgicon'></img>
-                </div>
-                <img src={SendMsgIcon.src} alt='SendMsgIcon'></img>
-            </div>
-            </>
-        );
-    return (
-        <>
-            <div className={Styles.ChatMsgSendBoxContainer}>
-            <div className={Styles.ChatMsgSendBox}>
-                <input type={'text'} placeholder='Message' onInput={isTypingHandler}></input>
-                <img src={InviteGameicon.src} alt='SendMsgicon'></img>
-            </div>
+  return (
+    <>
+      <div className={Styles.ChatMsgSendBoxContainer}>
+        <div className={Styles.ChatMsgSendBox}>
+          <input
+            type={"text"}
+            placeholder="Message"
+            value={EnteredMsg}
+            onChange={(e) => setEnteredMsg(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.keyCode === 13) {
+                console.log("Enter key is pressed");
+              }
+            }}
+          ></input>
+          <img src={InviteGameicon.src} alt="SendMsgicon"></img>
         </div>
-        </>
-    );
+        {EnteredMsg.length > 0 ? (
+          <img
+            src={SendMsgIcon.src}
+            alt="SendMsgIcon"
+            onClick={(e) => {
+              if (EnteredMsg.length > 1) console.log("send mssg");
+            }}
+          ></img>
+        ) : null}
+      </div>
+    </>
+  );
 };
