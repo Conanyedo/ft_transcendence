5fd49baf2124129cef48f92b827ed979c6a1a0bc
0xOd3n <abdellahbelhachmi13915@gmail.com>
2023-01-03 19:01:08 +0100

    modify : create UserList and Member nested component

--- client/components/Chat/InserChannelMembers.tsx
+++ client/components/Chat/InserChannelMembers.tsx
@@ -4,100 +4,123 @@ import { useState } from "react";
 import { words } from "lodash";
 import { composeWithDevTools } from "@reduxjs/toolkit/dist/devtoolsExtension";
 import { current } from "@reduxjs/toolkit";
+import Login from "@components/LoginPage/Login";
 
 class friend {
   fullName: string = "";
   avatar: string = "";
+  login: string = "";
 }
 
 const friends: friend[] = [
   {
     fullName: "Roronoa Zoro",
     avatar: "https://i.ytimg.com/vi/Fe3M_hgEDjk/maxresdefault.jpg",
+    login: "zoro",
   },
   {
     fullName: "Sakazuki Akaino",
     avatar:
       "https://i.pinimg.com/originals/2e/94/a0/2e94a0d1a109f506d542decca10af75e.jpg",
+    login: "akainu",
   },
   {
     fullName: "Monkey D Luffy",
     avatar:
       "https://img.assinaja.com/upl/lojas/mundosinfinitos/imagens/foto-one-piece.png",
+    login: "luffy",
   },
   {
     fullName: "Kozuki Oden",
     avatar:
       "https://nintendoeverything.com/wp-content/uploads/OPPW4-DLC_12-10-20.jpg",
+    login: "oden",
   },
 ];
 
-
-export const InsertChannelMembers = () => {
-  const [Search, setSearch] = useState<string>("");
-  const [Members, setMembers] = useState<friend[]>([]);
-  
-  const Member = (user: friend) => {
-    return (
-      <div className={Styles.MemberContainer}>
-        {user.fullName}
-        <img src={CloseIcon.src} onClick={(e) => RemoveMemberHandler(user)}></img>
-      </div>
-    );
-  };
-
-  const SearchinputHandler = (event: any) => {
-    setSearch(event.target.value);
+const UsersList: React.FC<any> = (props) => {
+  const AddMemberHandler = (user: friend) => {
+    props.setMembers([...props.Members, user]);
   };
   
-  const RemoveMemberHandler = (user : friend) => {
-    setMembers(current => current.filter(Members => {
-      return Members.fullName !== user.fullName
-    }))
-  };
-
-  const AddMemberHandler = (user : friend) => {
-    setMembers([...Members, user]);
-  };
 
   return (
-    <>
-      <div className={Styles.InsertChnlMembersContainer}>
-        {Members.map((user) => {
-          return Member(user);
-        })}
-        <input
-          type={"text"}
-          name="channelMembers"
-          // placeholder="Search"
-          onChange={SearchinputHandler}
-        ></input>
-      </div>
-      {Search && (
-        <div className={Styles.UserList}>
-          {friends.map((user: friend) => {
-            if (user.fullName.toUpperCase().includes(Search.toUpperCase()))
-            return (
-              <>
-              { !Members.includes(user) &&
+    <div className={Styles.UserList}>
+      {friends.map((user: friend) => {
+        if (user.fullName.toUpperCase().includes(props.Search.toUpperCase()))
+          return (
+            <>
+              {!props.Members.includes(user) && (
                 <div className={Styles.UserContainer}>
                   <div className={Styles.UserInfo}>
                     <img src={user.avatar}></img>
                     <p>{user.fullName}</p>
                   </div>
-                    <div
-                      className={Styles.AddUserBtn}
-                      onClick={(e) => AddMemberHandler(user)}
-                    >
-                      Add
-                    </div>
+                  <div
+                    className={Styles.AddUserBtn}
+                    onClick={(e) => AddMemberHandler(user)}
+                  >
+                    Add
+                  </div>
                 </div>
-              }
-              </>
+              )}
+            </>
+          );
+      })}
+    </div>
+  );
+};
+
+const Member: React.FC<any> = (props) => {
+  return (
+    <div className={Styles.MemberContainer}>
+      {props.member.fullName}
+      <img
+        src={CloseIcon.src}
+        onClick={(e) => props.removeMember(props.member)}
+      ></img>
+    </div>
+  );
+};
+
+export const InsertChannelMembers = () => {
+  const [Search, setSearch] = useState<string>("");
+  const [Members, setMembers] = useState<friend[]>([]);
+
+  const SearchinputHandler = (event: any) => {
+    setSearch(event.target.value);
+  };
+
+  const RemoveMemberHandler = (user: friend) => {
+    setMembers((current) =>
+      current.filter((member) => {
+        return member.fullName !== user.fullName;
+      })
+    );
+  };
+
+  return (
+    <>
+      <label htmlFor="inputAddMember">
+        <div className={Styles.InsertChnlMembersContainer}>
+          {Members.map((member) => {
+            return (
+              <Member
+                removeMember={RemoveMemberHandler}
+                member={member}
+              />
             );
           })}
+          <input
+            id="inputAddMember"
+            type={"text"}
+            name="channelMembers"
+            placeholder="Add User"
+            onChange={SearchinputHandler}
+          ></input>
         </div>
-      )}
+      </label>
+      { <UsersList Members={Members} setMembers={setMembers} Search={Search}/>}
     </>
   );
 };
