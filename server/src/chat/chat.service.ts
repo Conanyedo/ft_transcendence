b0d931e47b2adf351dfd1b9466e4a5d8ba1a8188
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2023-01-11 20:57:02 +0100

    add unread message functionality

--- server/src/chat/chat.service.ts
+++ server/src/chat/chat.service.ts
@@ -53,13 +53,25 @@ export class ChatService {
 		return { data: [...convsList] };
 	}
 
+	async updateUnreadMsgs(login: string, convId: string) {
+		const exist = await this.memberRepository
+			.query(`select members.id, members."unread" from members Join users ON members."userId" = users.id where members."conversationId" = '${convId}' AND users."login" = '${login}' AND members."leftDate" is null;`);
+		if (!exist.length) return;
+		console.log('unread: ', exist[0]);
+		const unread: number = +exist[0].unread;
+		await this.memberRepository
+			.query(`update members set unread = '${unread + 1}' where members."id" = '${exist[0].id}';`);
+	}
+
 	async getRoomSockets(login: string, room: string) {
 		const sockets = await this.chatGateway.server.in(room).fetchSockets();
 		const newSockets: string[] = [];
 		await Promise.all(sockets.map(async (socket) => {
 			const relation = await this.friendshipService.getRelation(login, socket.data.login);
-			if (relation !== 'blocked')
+			if (relation !== 'blocked') {
+				if (socket.data.login !== login) await this.updateUnreadMsgs(socket.data.login, room)
 				newSockets.push(socket.id);
+			}
 		}))
 		return [...newSockets];
 	}
@@ -134,30 +146,40 @@ export class ChatService {
 		return await bcrypt.compare(input, password);
 	}
 
+	async getConvInfo(login: string, conv: conversationDto) {
+		conv.lastUpdate = conv.leftDate ? conv.leftDate : conv.lastUpdate;
+		const convInfo: conversationDto = { ...conv }
+		if (conv.type === 'Dm') {
+			const users = await this.memberRepository
+				.query(`select users.login, users.fullname, users.status, users.avatar, members."unread" from members Join users ON members."userId" = users.id where members."conversationId" = '${conv.convId}' AND users.login != '${login}';`);
+			convInfo.name = users[0].fullname;
+			convInfo.login = users[0].login;
+			convInfo.status = users[0].status;
+			convInfo.avatar = users[0].avatar;
+			convInfo.relation = conv.status;
+		}
+		else {
+			const membersNum = await this.memberRepository
+				.query(`select COUNT(*) from members where members."conversationId" = '${convInfo.convId}' AND members."leftDate" is null;`);
+			convInfo.login = convInfo.name;
+			convInfo.membersNum = membersNum[0].count;
+			convInfo.relation = conv.status;
+		}
+		return convInfo;
+	}
+
+	async getConversation(login: string, convId: string) {
+		const convs: conversationDto[] = await this.memberRepository
+			.query(`select conversations.id as "convId", conversations.type, conversations.avatar, conversations.name, conversations."lastUpdate", members.status, members."leftDate", members."unread" from members Join users ON members."userId" = users.id Join conversations ON members."conversationId" = conversations.id where users."login" = '${login}' AND members."conversationId" = '${convId}';`);
+		if (!convs.length) return { err: 'Conversation not found!' };
+		const conversation: conversationDto = await this.getConvInfo(login, convs[0]);
+		return { data: { ...conversation } };
+	}
+
 	async getConversations(login: string) {
 		const convs: conversationDto[] = await this.memberRepository
 			.query(`select conversations.id as "convId", conversations.type, conversations.avatar, conversations.name, conversations."lastUpdate", members.status, members."leftDate" from members Join users ON members."userId" = users.id Join conversations ON members."conversationId" = conversations.id where users."login" = '${login}' order by conversations."lastUpdate" DESC;`);
-		const conversations: conversationDto[] = await Promise.all(convs.map(async (conv) => {
-			conv.lastUpdate = conv.leftDate ? conv.leftDate : conv.lastUpdate;
-			const convInfo: conversationDto = { ...conv }
-			if (conv.type === 'Dm') {
-				const users = await this.memberRepository
-					.query(`select users.login, users.fullname, users.status, users.avatar from members Join users ON members."userId" = users.id where members."conversationId" = '${conv.convId}' AND users.login != '${login}';`);
-				convInfo.name = users[0].fullname;
-				convInfo.login = users[0].login;
-				convInfo.status = users[0].status;
-				convInfo.avatar = users[0].avatar;
-				convInfo.relation = conv.status;
-			}
-			else {
-				const membersNum = await this.memberRepository
-					.query(`select COUNT(*) from members where members."conversationId" = '${convInfo.convId}' AND members."leftDate" is null;`);
-				convInfo.login = convInfo.name;
-				convInfo.membersNum = membersNum[0].count;
-				convInfo.relation = conv.status;
-			}
-			return convInfo;
-		}))
+		const conversations: conversationDto[] = await Promise.all(convs.map(async (conv) => (this.getConvInfo(login, conv))));
 		conversations.sort((a: conversationDto, b: conversationDto) => {
 			const right: number = a.lastUpdate.getTime();
 			const left: number = b.lastUpdate.getTime();
