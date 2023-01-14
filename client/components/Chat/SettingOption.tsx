0c01910c341c0d7cf1ad7b814ba85d4ae1e206e5
murasakibarachi <abdellahbelhachmi13915@gmail.com>
2023-01-14 04:21:03 +0100

    Add : SettingOption comporent

---
+++ client/components/Chat/SettingOption.tsx
@@ -0,0 +1,22 @@
+import styles from "@styles/Chat/ChatMessages.module.css";
+
+export const SettingOption = (props: {name : string, optionClickHandler : () => void}) => {
+  
+  const isred : boolean = (props.name === "Block user" || props.name === "Leave channel" || props.name === "Unblock user");
+  
+  return (
+    <>
+      <div
+        style={
+          isred
+            ? { color: "#FF6482"}
+            : {}
+        }
+        className={styles.SettingOptionContainer}
+        onClick={props.optionClickHandler}
+      >
+        {props.name}
+      </div>
+    </>
+  );
+};
