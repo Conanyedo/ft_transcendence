240589124519f9005b7787479ba1226d2cfb3e57
murasakibarachi <abdellahbelhachmi13915@gmail.com>
2023-01-15 19:57:44 +0100

    modify : add setshowmember state for close icon and animation

--- client/components/Chat/AddMembers.tsx
+++ client/components/Chat/AddMembers.tsx
@@ -1,10 +1,15 @@
 import Styles from "@styles/Chat/AddMembers.module.css";
 import { InsertChannelMembers } from "./InserChannelMembers";
 import CloseIcon from "@public/Cross.svg";
+import { Dispatch, SetStateAction } from "react";
+import { motion } from "framer-motion";
 
-export const AddMembers = () => {
+export const AddMembers = (props: {
+  setShowAddMember: Dispatch<SetStateAction<boolean>>;
+}) => {
   const closeAddMemberHandler = () => {
     console.log("close add member");
+    props.setShowAddMember(false);
   };
 
   const addMemberclickHandler = () => {
@@ -13,16 +18,36 @@ export const AddMembers = () => {
 
   return (
     <>
-      <div className={Styles.AddMemberContainer}>
-        <div className={Styles.AddMemberHeader}>
-          Add Member
-          <img src={CloseIcon.src} onClick={closeAddMemberHandler}></img>
-        </div>
-        <InsertChannelMembers />
-        <div className={Styles.AddMemberBtn} onClick={addMemberclickHandler}>
-          Add
-        </div>
-      </div>
+      <motion.div
+        className={Styles.AddMemberBackground}
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+      >
+        <motion.div
+          className={Styles.AddMemberContainer}
+          initial={{
+            opacity: 0,
+            scale: 0.1,
+          }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+          }}
+        >
+          <div className={Styles.AddMemberHeader}>
+            Add Member
+            <img src={CloseIcon.src} onClick={closeAddMemberHandler}></img>
+          </div>
+          <InsertChannelMembers />
+          <div className={Styles.AddMemberBtn} onClick={addMemberclickHandler}>
+            Add
+          </div>
+        </motion.div>
+      </motion.div>
     </>
   );
 };
