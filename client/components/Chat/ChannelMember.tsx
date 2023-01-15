f7b7304fc43565b4d24c4ebc7984d947f7b1b030
murasakibarachi <abdellahbelhachmi13915@gmail.com>
2023-01-15 20:16:36 +0100

    modify : add channel Member setting case and showMuteMember state for MuteMember popup

--- client/components/Chat/ChannelMember.tsx
+++ client/components/Chat/ChannelMember.tsx
@@ -1,38 +1,106 @@
-import Styles from '@styles/Chat/ChannelMember.module.css';
-import ThreeDots from '@public/Chat/ThreeDots.svg';
-import { SettingOption } from './SettingOption';
-import { useState } from 'react';
-import { channelMembers, member } from './ChatChnlProfile';
+import Styles from "@styles/Chat/ChannelMember.module.css";
+import ThreeDots from "@public/Chat/ThreeDots.svg";
+import { SettingOption } from "./SettingOption";
+import { useState } from "react";
+import { member } from "./ChatChnlProfile";
+import { motion } from "framer-motion";
+import { MuteMember } from "./MuteMember";
+import { AddMembers } from "./AddMembers";
+interface Props {
+  relation: string;
+  role: string;
+  member: member;
+}
 
-export const ChannelMember = (props : {relation : string, role : string, member : member}) => {
-    const [showChnlMemberSettings, setShowChnlMemberSettings] = useState<boolean>(false);
-    const loggedInUsr = localStorage.getItem("owner");
+export const ChannelMember: React.FC<Props> = ({ relation, role, member }) => {
+  const [showChnlMemberSettings, setShowChnlMemberSettings] =
+    useState<boolean>(false);
+  const [showMuteMember, setShowMuteMember] = useState<boolean>(false);
+  const loggedInUsr = localStorage.getItem("owner");
 
-    const MemberSettingClickHandler = () => {
-        console.log('Member setting ');
-    };
+  const dismissAdminClickHandler = () => {
+    console.log("Dismiss Admin ");
+  };
 
-    console.log("relation member :", props.relation);
-    // console.log("role member :", props.role);
-    // console.log("member :", props.member);
-    return (
-        <>
-            <div className={Styles.ChannelMemberContainer}>
-                <div className={Styles.ChannelMemberProfile}>
-                    <img src={props.member.avatar}></img>
-                    {(loggedInUsr === props.member.login) ? "You" : props.member.fullName}
-                </div>
-                {/* { (props.relation === "Owner" || props.relation === "Admin") && */}
-                    <img src={ThreeDots.src} onClick={() => setShowChnlMemberSettings(!showChnlMemberSettings)}/>
-                {showChnlMemberSettings && (
-              <div className={Styles.SettingContainer}>
-                {props.relation === "Owner" ||
-                props.relation === "Admin" ? (
-                  <SettingOption name={"Settings"} optionClickHandler={() => console.log("clicked opt")}/>
-                ) : null}
-              </div>
+  const makeAdminClickHandler = () => {
+    console.log("Make admin");
+  };
+
+  const muteMemberClickHandler = () => {
+    console.log("Mute Member");
+    setShowMuteMember(true);
+  };
+
+  const unmuteMemberClickHandler = () => {
+    console.log("Unmute Member");
+  };
+
+  const removeMemberClickHandler = () => {
+    console.log("Remove Member");
+  };
+
+  console.log("relation member :", relation);
+  console.log("role member :", role);
+
+  return (
+    <>
+      {showMuteMember && <MuteMember setShowMuteMember={setShowMuteMember}/>}
+      <div className={Styles.ChannelMemberContainer}>
+        <div className={Styles.ChannelMemberProfile}>
+          <img src={member.avatar}></img>
+          {loggedInUsr === member.login ? "You" : member.fullName}
+        </div>
+
+        {(relation === "Owner" && role !== "Owners") ||
+        (relation === "Admin" && (role === "Members" || role === "Muted")) ? (
+          <div className={Styles.ChatMsgSettings}>
+            <img
+              src={ThreeDots.src}
+              onClick={() => setShowChnlMemberSettings(!showChnlMemberSettings)}
+            />
+            {showChnlMemberSettings && (
+              <motion.div
+                className={Styles.SettingContainer}
+                initial={{
+                  opacity: 0,
+                  scale: 0.1,
+                }}
+                animate={{
+                  opacity: 1,
+                  scale: 1,
+                }}
+              >
+                {role === "Admins" ? (
+                  <SettingOption
+                    name={"Dismiss admin"}
+                    optionClickHandler={dismissAdminClickHandler}
+                  />
+                ) : role !== "Muted" ? (
+                  <>
+                    <SettingOption
+                      name="Mute member"
+                      optionClickHandler={muteMemberClickHandler}
+                    />
+                    <SettingOption
+                      name="Make as admin"
+                      optionClickHandler={makeAdminClickHandler}
+                    />
+                  </>
+                ) : (
+                  <SettingOption
+                    name="Unmute member"
+                    optionClickHandler={unmuteMemberClickHandler}
+                  />
+                )}
+                <SettingOption
+                  name="Remove member"
+                  optionClickHandler={removeMemberClickHandler}
+                />
+              </motion.div>
             )}
-            </div>
-        </>
-    );
+          </div>
+        ) : null}
+      </div>
+    </>
+  );
 };
