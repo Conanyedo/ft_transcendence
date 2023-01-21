2c21d2adbfee901b46dd9052753f0bf5602bc155
murasakibarachi <abdellahbelhachmi13915@gmail.com>
2023-01-21 20:23:46 +0100

    modify

--- client/components/Chat/ChatConversations.tsx
+++ client/components/Chat/ChatConversations.tsx
@@ -1,29 +1,83 @@
 import Styles from "@styles/Chat/ChatConversations.module.css";
-import { useState, Dispatch, SetStateAction } from "react";
+import {
+  useState,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useLayoutEffect,
+} from "react";
 import Searchicon from "../../public/SearchIcon.svg";
 import Addchannel from "../../public/Chat/AddChannel.svg";
 import Addchannelselected from "../../public/Chat/AddChannelSelected.svg";
-import { conversations} from "@Types/dataTypes";
+import { ChannelData, conversations, initialconv, MsgData } from "@Types/dataTypes";
 import { Conversation } from "./Conversation";
 import { CreateChannel } from "./CreateChannel";
+import { fetchDATA } from "@hooks/useFetchData";
+import { useRouter } from "next/router";
 
-// import { ChannelAsset, BlueChannelAsset } from "@svg/index";
+const initialChnlState: ChannelData = {
+  avatar: "",
+  name: "",
+  type: "Public",
+  password: "",
+  members: [],
+};
 
 interface Props {
   isMobile: boolean;
-  convs: conversations[];
-  selectedConv: number;
-  setselectedConv: Dispatch<SetStateAction<number>>;
+  convs : conversations[];
+  setConvData: Dispatch<SetStateAction<conversations>>;
 }
 
 export const ChatConversations: React.FC<Props> = ({
   isMobile,
-  convs,
-  selectedConv,
-  setselectedConv,
+  convs,  
+  setConvData,
 }) => {
   const [showAddChannel, setshowAddChannel] = useState<boolean>(false);
   const [searchConv, setsearchConv] = useState<string>("");
+  const [selectedConv, setselectedConv] = useState<string | string[]>("0");
+  const router = useRouter();
+
+
+  /* -------------------------------------------------------------------------- */
+  /*                      get the query from route if exist                     */
+  /* -------------------------------------------------------------------------- */
+
+  useLayoutEffect(() => {
+    if (router.query.login) {
+      const id = router.query.login;
+      setselectedConv(id);
+    } else if (router.query.channel) {
+      const id = router.query.channel;
+      setselectedConv(id);
+    } else {
+      setselectedConv("0");
+    }
+  }, [router.query]);
+
+  /* -------------------------------------------------------------------------- */
+  /*                             check if conv exist                            */
+  /* -------------------------------------------------------------------------- */
+
+  useLayoutEffect(() => {
+    if (convs.length > 0) {
+      const foundconv = convs.find((conv) => conv.convId === selectedConv);
+      if (foundconv) {
+        setConvData(foundconv);
+      } else setConvData(initialconv);
+      if (selectedConv !== "0")
+        foundconv?.type === "Dm"
+          ? router.replace({
+              pathname: `/chat`,
+              query: { login: selectedConv },
+            })
+          : router.replace({
+              pathname: `/chat`,
+              query: { channel: selectedConv },
+            });
+    }
+  }, [selectedConv, convs]);
 
   const AddChannelClickHandler = () => {
     setshowAddChannel(true);
@@ -35,14 +89,18 @@ export const ChatConversations: React.FC<Props> = ({
 
   return (
     <>
-      {showAddChannel ? (
-        <CreateChannel isUpdate={false} CloseChannelHandler={CloseChannelHandler} />
-      ) : null}
+      {showAddChannel && (
+        <CreateChannel
+          isUpdate={false}
+          initialChnlState={initialChnlState}
+          CloseChannelHandler={CloseChannelHandler}
+        />
+      )}
       <div
         className={Styles.ChatConversations}
         style={
           isMobile
-            ? selectedConv === 0
+            ? selectedConv === "0"
               ? { width: "100%" }
               : { width: "0" }
             : {}
@@ -71,17 +129,19 @@ export const ChatConversations: React.FC<Props> = ({
         </div>
         <div className={Styles.Conversationlist}>
           {convs.map((conv: conversations) => {
-            if (conv.fullName.toUpperCase().includes(searchConv.toUpperCase()))
+            if (conv.name.toUpperCase().includes(searchConv.toUpperCase()))
               return (
                 <Conversation
-                  key={conv.convid}
+                  key={conv.convId}
+                  convId={conv.convId}
                   avatar={conv.avatar}
-                  fullName={conv.fullName}
+                  name={conv.name}
                   membersNum={conv.membersNum}
                   status={conv.status}
-                  read={conv.read}
-                  selected={selectedConv === conv.convid}
-                  onClick={() => setselectedConv(conv.convid)}
+                  unread={conv.unread}
+                  type={conv.type}
+                  selected={selectedConv === conv.convId}
+                  setSelectedConv={setselectedConv}
                 />
               );
           })}
