1cd4e4527f8ac3f0f9dca8428ad2f268ac199016
murasakibarachi <abdellahbelhachmi13915@gmail.com>
2023-01-21 20:19:52 +0100

    modify : change usestate to usereducer for insertchannelmember

--- client/components/Chat/AddMembers.tsx
+++ client/components/Chat/AddMembers.tsx
@@ -1,12 +1,22 @@
 import Styles from "@styles/Chat/AddMembers.module.css";
 import { InsertChannelMembers } from "./InserChannelMembers";
 import CloseIcon from "@public/Cross.svg";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useReducer } from "react";
 import { motion } from "framer-motion";
+
+
+const reducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "members":
+      return { ...state, members: action.members };
+  }
+};
+
 
 export const AddMembers = (props: {
   setShowAddMember: Dispatch<SetStateAction<boolean>>;
 }) => {
+  const [state, dispatch] = useReducer(reducer, []);
   const closeAddMemberHandler = () => {
     console.log("close add member");
     props.setShowAddMember(false);
@@ -42,7 +52,7 @@ export const AddMembers = (props: {
             Add Member
             <img src={CloseIcon.src} onClick={closeAddMemberHandler}></img>
           </div>
-          <InsertChannelMembers />
+          <InsertChannelMembers state={state} dispatch={dispatch}/>
           <div className={Styles.AddMemberBtn} onClick={addMemberclickHandler}>
             Add
           </div>
