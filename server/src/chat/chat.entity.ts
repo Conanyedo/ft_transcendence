863b8e1700f587fbe59abc4aaed55fa653946469
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2023-01-27 22:34:08 +0100

    set lastUpdate as updateDateColumn in conversations

--- server/src/chat/chat.entity.ts
+++ server/src/chat/chat.entity.ts
@@ -34,7 +34,7 @@ export class Conversation {
 	@Column({ default: null })
 	name: string;
 
-	@Column()
+	@UpdateDateColumn()
 	lastUpdate: Date;
 
 	@Column({ type: 'enum', enum: convType, default: convType.Dm })
