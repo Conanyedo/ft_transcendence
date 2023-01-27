2d5c36a0a6449712455552d85f00d39b895719d3
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2023-01-27 15:29:02 +0100

    implement handleRequest method in guard to catch erro
    redirect to home page in case of failed authentication

--- server/src/auth/google-oauth.guard.ts
+++ server/src/auth/google-oauth.guard.ts
@@ -1,5 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class GoogleOauthGuard extends AuthGuard('google') { }
+export class GoogleOauthGuard extends AuthGuard('google') {
+	constructor(
+		private readonly configService: ConfigService
+	) {
+		super()
+	}
+
+	handleRequest<TUser = any>(err: any, user: any, info: any, context: ExecutionContext, status?: any): TUser {
+		if (err || !user) {
+			const ctx = context.switchToHttp();
+			const response = ctx.getResponse();
+			response.redirect(`http://${this.configService.get('CLIENT_IP')}`)
+		}
+		return user;
+	}
+}
--- server/src/auth/intra-auth.guard.ts
+++ server/src/auth/intra-auth.guard.ts
@@ -1,5 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class IntraAuthGuard extends AuthGuard('42') { }
+export class IntraAuthGuard extends AuthGuard('42') {
+	constructor(
+		private readonly configService: ConfigService
+	) {
+		super()
+	}
+
+	handleRequest<TUser = any>(err: any, user: any, info: any, context: ExecutionContext, status?: any): TUser {
+		if (err || !user) {
+			const ctx = context.switchToHttp();
+			const response = ctx.getResponse();
+			response.redirect(`http://${this.configService.get('CLIENT_IP')}`)
+		}
+		return user;
+	}
+}
