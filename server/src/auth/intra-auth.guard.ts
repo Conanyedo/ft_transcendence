382e5d96be7ebaa636c00098f5457948f11d41df
younes bouddou <54075373+Conanyedo@users.noreply.github.com>
2023-01-28 23:33:05 +0100

    use exceptionFilter instead of requestHanlder in guard

--- server/src/auth/intra-auth.guard.ts
+++ server/src/auth/intra-auth.guard.ts
@@ -3,20 +3,4 @@ import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class IntraAuthGuard extends AuthGuard('42') {
-	constructor(
-		private readonly configService: ConfigService
-	) {
-		super()
-	}
-
-	handleRequest<TUser = any>(err: any, user: any, info: any, context: ExecutionContext, status?: any): TUser {
-		if (err && !user) {
-			const ctx = context.switchToHttp();
-			const response = ctx.getResponse();
-			response.redirect(`http://${this.configService.get('CLIENT_IP')}`)
-			return response.status(HttpStatus.UNAUTHORIZED).send();
-		}
-		return user;
-	}
-}
+export class IntraAuthGuard extends AuthGuard('42') { }
